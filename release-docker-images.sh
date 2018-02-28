28ceb5fc8edbf8eccfbbc6bb7c2f9086a5b22b71
Bart Jeukendrup <bart@jeukendrup.nl>
2018-02-28 15:43:04 +0100

    Also add latest Docker tag to images

--- release-docker-images.sh
+++ release-docker-images.sh
@@ -6,28 +6,32 @@ set -x # echo commands
 RELEASE_TAG=${RELEASE_TAG:-latest}
 
 docker build \
-	-t nlxio/docs:${RELEASE_TAG} \
+	-t nlxio/docs:latest -t nlxio/docs:${RELEASE_TAG} \
 	-f docs/Dockerfile .
 
 docker build \
-	-t nlxio/unsafe-ca:${RELEASE_TAG} \
+	-t nlxio/unsafe-ca:latest -t nlxio/unsafe-ca:${RELEASE_TAG} \
 	-f unsafe-ca/Dockerfile .
 
 docker build \
-	-t nlxio/directory:${RELEASE_TAG} \
+	-t nlxio/directory:latest -t nlxio/directory:${RELEASE_TAG} \
 	-f directory/Dockerfile .
 
 docker build \
-	-t nlxio/inway:${RELEASE_TAG} \
+	-t nlxio/inway:latest -t nlxio/inway:${RELEASE_TAG} \
 	-f inway/Dockerfile .
 
 docker build \
-	-t nlxio/outway:${RELEASE_TAG} \
+	-t nlxio/outway:latest -t nlxio/outway:${RELEASE_TAG} \
 	-f outway/Dockerfile .
 
 docker build \
 	-t nlxio/directory-ui:${RELEASE_TAG} \
 	-f directory-ui/Dockerfile .
+
+docker build \
+	-t nlxio/certportal:latest -t nlxio/certportal:${RELEASE_TAG} \
+	-f certportal/Dockerfile .
 
 # TODO: only push the image when this script is ran in CI/CD or forced using env var (backup-plan for when CI/CD is down/unavailable)
 docker push nlxio/docs:${RELEASE_TAG}
