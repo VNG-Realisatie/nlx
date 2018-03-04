030b1e7525119276fbbc5887cbe15a307235b78e
Bart Jeukendrup <bart@jeukendrup.nl>
2018-03-04 18:23:08 +0100

    Also push latest tags

--- release-docker-images.sh
+++ release-docker-images.sh
@@ -26,7 +26,7 @@ docker build \
 	-f outway/Dockerfile .
 
 docker build \
-	-t nlxio/directory-ui:${RELEASE_TAG} \
+	-t nlxio/directory-ui:latest -t nlxio/directory-ui:${RELEASE_TAG} \
 	-f directory-ui/Dockerfile .
 
 docker build \
@@ -34,10 +34,17 @@ docker build \
 	-f certportal/Dockerfile .
 
 # TODO: only push the image when this script is ran in CI/CD or forced using env var (backup-plan for when CI/CD is down/unavailable)
+docker push nlxio/docs:latest
 docker push nlxio/docs:${RELEASE_TAG}
+docker push nlxio/unsafe-ca:latest
 docker push nlxio/unsafe-ca:${RELEASE_TAG}
+docker push nlxio/directory:latest
 docker push nlxio/directory:${RELEASE_TAG}
+docker push nlxio/inway:latest
 docker push nlxio/inway:${RELEASE_TAG}
+docker push nlxio/outway:latest
 docker push nlxio/outway:${RELEASE_TAG}
+docker push nlxio/certportal:latest
 docker push nlxio/certportal:${RELEASE_TAG}
+docker push nlxio/directory-ui:latest
 docker push nlxio/directory-ui:${RELEASE_TAG}
