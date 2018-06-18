04b1ad17459d516e2b0572bbde20cfae2b8dd633
Geert-Johan Riemer <geertjohan@geertjohan.net>
2018-06-18 15:36:11 +0200

    inway: re-register to directory every 10 seconds

--- inway/inway.go
+++ inway/inway.go
@@ -95,27 +95,31 @@ func (i *Inway) AddServiceEndpoint(s ServiceEndpoint, documentationURL string) e
 }
 
 func (i *Inway) announceToDirectory(s ServiceEndpoint, documentationURL string) {
-	for {
-		resp, err := i.directoryClient.RegisterInway(context.Background(), &directoryapi.RegisterInwayRequest{
-			InwayAddress: i.selfAddress,
-			Services: []*directoryapi.RegisterInwayRequest_RegisterService{
-				{
-					Name:             s.ServiceName(),
-					DocumentationUrl: documentationURL,
+	go func() {
+		for {
+			resp, err := i.directoryClient.RegisterInway(context.Background(), &directoryapi.RegisterInwayRequest{
+				InwayAddress: i.selfAddress,
+				Services: []*directoryapi.RegisterInwayRequest_RegisterService{
+					{
+						Name:             s.ServiceName(),
+						DocumentationUrl: documentationURL,
+					},
 				},
-			},
-		})
-		if err != nil {
-			if errStatus, ok := status.FromError(err); ok && errStatus.Code() == codes.Unavailable {
-				i.logger.Info("waiting for director...")
-				time.Sleep(1 * time.Second)
-				continue
+			})
+			if err != nil {
+				if errStatus, ok := status.FromError(err); ok && errStatus.Code() == codes.Unavailable {
+					i.logger.Info("waiting for director...")
+					time.Sleep(1 * time.Second)
+					continue
+				}
+				i.logger.Error("failed to register to directory", zap.Error(err))
 			}
-			i.logger.Error("failed to register to directory", zap.Error(err))
+			if resp != nil && resp.Error != "" {
+				i.logger.Error(fmt.Sprintf("failed to register to directory: %s", resp.Error))
+			}
+			
+			// sleep 10 seconds before re-registering
+			time.Sleep(10*time.Second)
 		}
-		if resp != nil && resp.Error != "" {
-			i.logger.Error(fmt.Sprintf("failed to register to directory: %s", resp.Error))
-		}
-		break
-	}
+	}()
 }
