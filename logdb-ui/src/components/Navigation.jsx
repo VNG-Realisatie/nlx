5479c0fdc09aeae6402f691586c0423525c097e0
Bart <bart@jeukendrup.nl>
2018-06-19 18:14:21 +0200

    Initial commit for logs-db-ui

---
+++ logdb-ui/src/components/Navigation.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import logo from '../assets/images/logo.svg'
+import menu from '../assets/icons/menu.svg'
+import github from '../assets/icons/github.svg'
+import {Link} from 'react-router-dom'
+
+export default class Navigation extends React.Component {
+    render() {
+        return (
+            <header className="navbar fixed-top bg-white navbar-expand-md">
+                <div className="container">
+                    <a className="navbar-logo d-md-none" href="">
+                        <img src={logo} alt="logo" />
+                    </a>
+                    <button className="navbar-toggler navbar-logo d-md-none" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <img src={menu} alt="menu" />
+                    </button>
+
+                    <nav className="collapse navbar-collapse order-last order-md-first" id="navbarSupportedContent" aria-label="Page navigation">
+                        <ul className="navbar-nav flex-row-md">
+                            <li className="nav-item d-none d-md-block">
+                                <a className="navbar-logo" href="">
+                                    <img src={logo} alt="logo" />
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <ul className="navbar-nav flex-row ml-md-auto">
+                        <li className="nav-item">
+                            <a className="nav-link p-2" href="https://github.com/VNG-Realisatie/nlx" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+                                <img src={github} alt="logo" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </header>
+        )
+    }
+}
