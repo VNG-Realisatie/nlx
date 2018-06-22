e37b4e32b03c49c438b316c92f6599e3cf0c8767
Bart <bart@jeukendrup.nl>
2018-06-22 13:30:39 +0200

    Some bugfixes on directory-ui and logdb-ui

--- directory-ui/src/index.js
+++ directory-ui/src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import 'bootstrap/dist/js/bootstrap.js';
-
 import 'core-js/fn/string/includes';
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
