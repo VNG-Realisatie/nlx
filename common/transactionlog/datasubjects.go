3f708deb2848503b42e9eceaeea6621cd8f524da
Ronald Koster <ronald.koster07@gmail.com>
2018-10-02 18:03:18 +0200

    common: added unittests to derrsa and transactionlog/datasubjects common: using regex to check the key value pairs of the datasubjects

--- common/transactionlog/datasubjects.go
+++ common/transactionlog/datasubjects.go
@@ -1,28 +1,35 @@
 package transactionlog
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/pkg/errors"
 )
 
+var regExpKey = regexp.MustCompile("^[a-z0-9]{4,100}$")
+var regExpValue = regexp.MustCompile("^[a-zA-Z0-9.-]{1,100}$")
+
 // ParseDataSubjectHeader parses a data subject header value and returns a key/value map, or an error.
 func ParseDataSubjectHeader(header string) (map[string]string, error) {
+	if len(header) == 0 {
+		return make(map[string]string), nil
+	}
 	parts := strings.Split(header, ",")
 	dataSubjects := make(map[string]string)
 	for i, part := range parts {
-		part = strings.TrimSpace(part)
-		if part == "" {
-			return nil, errors.Errorf("invalid datasubject in header, %dth subject is empty", i+1)
-		}
-		//TODO: Add regex to validate key and value
-		// key: [a-z-]{4,100}
-		// value: all printable characters? {1,100}?
 		kv := strings.Split(part, "=")
 		if len(kv) != 2 {
 			return nil, errors.Errorf("invalid datasubject in header, %dth subject is not a correct key=value format", i+1)
 		}
-		key, value := kv[0], kv[1]
+		key, value := strings.TrimSpace(kv[0]), strings.TrimSpace(kv[1])
+		if !regExpKey.MatchString(key) {
+			return nil, errors.Errorf("invalid datasubject in header, %dth subject key '%s' is not valid", i+1, key)
+		}
+
+		if !regExpValue.MatchString(value) {
+			return nil, errors.Errorf("invalid datasubject in header, %dth subject value '%s' is not valid", i+1, value)
+		}
 		dataSubjects[key] = value
 	}
 	return dataSubjects, nil
