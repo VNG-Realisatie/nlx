f9b4088e50ae80fccd3738afb002bd08fd9bf0c1
Bart Jeukendrup <bart@jeukendrup.nl>
2018-11-02 15:39:04 +0100

    Applied some more linting

--- directory-ui/src/components/Spinner.js
+++ directory-ui/src/components/Spinner.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import './Spinner.css';
+import './Spinner.css'
 
 class Spinner extends Component {
-  render() {
-    return (
-      <div className="app-loader">
-        <div className="lds-roller">
-          <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app-loader">
+                <div className="lds-roller">
+                    <div />
+                    <div />
+                    <div />
+                    <div />
+                    <div />
+                    <div />
+                    <div />
+                    <div />
+                </div>
+            </div>
+        )
+    }
 }
 
-export default Spinner;
+export default Spinner
