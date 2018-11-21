e2846b514845387fb8c0a06f0222a45b8a213e2d
Geert-Johan Riemer <geertjohan@geertjohan.net>
2018-11-21 15:14:28 +0100

    common/orgtls: improve error messages

--- common/orgtls/load.go
+++ common/orgtls/load.go
@@ -17,22 +17,22 @@ func Load(options TLSOptions) (*x509.CertPool, *x509.Certificate, error) {
 
 	certPEM, err := ioutil.ReadFile(options.OrgCertFile)
 	if err != nil {
-		return nil, nil, errors.Wrapf(err, "failed to read certificate PEM file `%s`", options.OrgCertFile)
+		return nil, nil, errors.Wrapf(err, "failed to open and read organization certificate file `%s`", options.OrgCertFile)
 	}
 	block, _ := pem.Decode(certPEM)
 	if block == nil {
-		return nil, nil, errors.New("failed to parse certificate PEM")
+		return nil, nil, errors.Errorf("failed to parse PEM for organization certificate `%s`", options.OrgCertFile)
 	}
 	cert, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to parse certificate")
+		return nil, nil, errors.Wrapf(err, "failed to parse x509 for organization certificate `%s`", options.OrgCertFile)
 	}
 
 	opts := x509.VerifyOptions{
 		Roots: roots,
 	}
 	if _, err := cert.Verify(opts); err != nil {
-		return nil, nil, errors.Wrap(err, "failed to verify certificate")
+		return nil, nil, errors.Wrap(err, "failed to verify certificate: not signed by root CA")
 	}
 
 	return roots, cert, nil
@@ -43,11 +43,11 @@ func LoadRootCert(rootCertFile string) (*x509.CertPool, error) {
 	roots := x509.NewCertPool()
 	rootPEM, err := ioutil.ReadFile(rootCertFile)
 	if err != nil {
-		return nil, errors.Wrapf(err, "failed to read CA root PEM file `%s`", rootCertFile)
+		return nil, errors.Wrapf(err, "failed to read root CA certificate file `%s`", rootCertFile)
 	}
 	ok := roots.AppendCertsFromPEM(rootPEM)
 	if !ok {
-		return nil, errors.Errorf("failed to parse CA root PEM from file `%s`", rootCertFile)
+		return nil, errors.Errorf("failed to parse PEM for root certificate `%s`", rootCertFile)
 	}
 	return roots, nil
 }
