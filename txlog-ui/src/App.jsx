1719e8a66c8f2c0ecb188b34ae779e3e5fa949f5
Niels Dequeker <niels.dequeker@fastmail.com>
2019-01-04 14:54:09 +0100

    #418 Fix import

--- txlog-ui/src/App.jsx
+++ txlog-ui/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Navigation from './components/Navigation'
+import Navigation from './components/Navigation/Navigation'
 import OverviewPage from './pages/OverviewPage/OverviewPage'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
