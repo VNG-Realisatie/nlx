43d6cad708776850f8eca75efdae0a596d87ec38
Niels Dequeker <niels.dequeker@fastmail.com>
2019-01-04 17:55:36 +0100

    #418 Simplify App & ErrorPage components

--- txlog-ui/src/App.jsx
+++ txlog-ui/src/App.jsx
@@ -1,21 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navigation from './components/Navigation/Navigation'
 import OverviewPage from './pages/OverviewPage/OverviewPage'
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Router>
-                    <div>
-                        <Navigation />
-                        <Route exact path="/" component={OverviewPage} />
-                    </div>
-                </Router>
-            </div>
-        )
-    }
-}
+const App = () => (
+  <div className="App">
+      <Router>
+          <div>
+              <Navigation />
+              <Route exact path="/" component={OverviewPage} />
+          </div>
+      </Router>
+  </div>
+)
 
 export default App
--- txlog-ui/src/pages/ErrorPage/ErrorPage.jsx
+++ txlog-ui/src/pages/ErrorPage/ErrorPage.jsx
@@ -1,20 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import './ErrorPage.scss'
 
-class ErrorPage extends Component {
-    render() {
-        return (
-            <div className="app-error-page">
-                <h1>Failed to load information</h1>
-                <p>
-                    Requested information is not available.
-                    <br />
-                    We apologize for any inconvenience.
-                </p>
-            </div>
-        )
-    }
-}
+const ErrorPage = () => (
+  <div className="ErrorPage">
+    <h1>Failed to load information</h1>
+
+    <p>
+      Requested information is not available.
+      <br />
+      We apologize for any inconvenience.
+    </p>
+  </div>
+)
 
 export default ErrorPage
