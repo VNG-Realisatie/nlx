b9fecbea6847724568dd26feb7a08c9af8dd9770
Niels Dequeker <niels.dequeker@fastmail.com>
2019-01-04 17:55:17 +0100

    #418 Fix Navigation test by providing the Router context

--- txlog-ui/src/components/Navigation/Navigation.test.jsx
+++ txlog-ui/src/components/Navigation/Navigation.test.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { MemoryRouter } from 'react-router-dom';
 import Navigation from './Navigation'
 
 it('renders without crashing', () => {
     expect(() => {
         const div = document.createElement('div')
-        ReactDOM.render(<Navigation />, div)
+        ReactDOM.render(
+          <MemoryRouter>
+              <Navigation />
+          </MemoryRouter>
+          , div)
     }).not.toThrow()
 })
