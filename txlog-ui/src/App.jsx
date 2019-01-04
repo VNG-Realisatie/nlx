e50fdd0e49ac8240b6c644e225b31354f99e8484
Niels Dequeker <niels.dequeker@fastmail.com>
2019-01-04 17:05:33 +0100

    #418 💅

--- txlog-ui/src/App.jsx
+++ txlog-ui/src/App.jsx
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navigation from './components/Navigation/Navigation'
 import OverviewPage from './pages/OverviewPage/OverviewPage'
-
-import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends Component {
     render() {
