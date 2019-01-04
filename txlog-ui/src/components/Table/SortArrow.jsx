ddfb3913833f47418e210d252e9e1ba3d9d5667f
Niels Dequeker <niels.dequeker@fastmail.com>
2019-01-04 15:33:48 +0100

    #418 Extract SortArrow from the Table

---
+++ txlog-ui/src/components/Table/SortArrow.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SortArrow = ({ sortAscending }) => (
+    <svg width="8" height="12" viewBox="0 0 8 12" name="sortingArrow">
+        <g id="arrow-down" fill="none" fillRule="evenodd">
+            <path
+                id="Shape"
+                fill="currentColor"
+                fillRule="nonzero"
+                transform={sortAscending ? 'rotate(90 4 5)' : 'rotate(-90 4 5)'}
+                d="M5 4h-6v2h6v3l4-4-4-4z"
+            />
+        </g>
+    </svg>
+)
+
+export default SortArrow
--- txlog-ui/src/components/Table/Table.jsx
+++ txlog-ui/src/components/Table/Table.jsx
@@ -1,26 +1,11 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import TableRow from './TableRow'
+import SortArrow from './SortArrow'
 
 class Table extends Component {
     render() {
         const { heads, rows, sortBy, sortAscending } = this.props
-
-        const sortArrow = (
-            <svg width="8" height="12" viewBox="0 0 8 12" name="sortingArrow">
-                <g id="arrow-down" fill="none" fillRule="evenodd">
-                    <path
-                        id="Shape"
-                        fill="currentColor"
-                        fillRule="nonzero"
-                        transform={
-                            sortAscending ? 'rotate(90 4 5)' : 'rotate(-90 4 5)'
-                        }
-                        d="M5 4h-6v2h6v3l4-4-4-4z"
-                    />
-                </g>
-            </svg>
-        )
 
         return (
             <div className="table-responsive mb-5">
@@ -48,7 +33,11 @@ class Table extends Component {
                                         }}
                                     >
                                         {col.label}{' '}
-                                        {sortBy === col.sortBy && sortArrow}
+                                        {sortBy === col.sortBy ? (
+                                            <SortArrow
+                                                sortAscending={sortAscending}
+                                            />
+                                        ) : null}
                                     </button>
                                 </th>
                             ))}
