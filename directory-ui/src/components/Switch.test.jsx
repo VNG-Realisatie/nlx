91a4357b6630aa80d3687449e6addde763f49854
Niels Dequeker <niels.dequeker@fastmail.com>
2019-01-07 10:08:27 +0100

    #417 Add smoke test for Switch component

---
+++ directory-ui/src/components/Switch.test.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Switch from './Switch'
+
+it('renders without crashing', () => {
+  expect(() => {
+    const div = document.createElement('div')
+    ReactDOM.render(<Switch />, div)
+  }).not.toThrow()
+})
