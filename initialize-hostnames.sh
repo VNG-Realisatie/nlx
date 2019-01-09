7b32182ecc0e8f6ff17e775d741f78bc799b93bf
Niels Dequeker <niels.dequeker@fastmail.com>
2019-01-09 14:18:00 +0100

    Move mapping of hostnames to shell file

---
+++ initialize-hostnames.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "$(minikube ip)                 traefik.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)            docs.dev.nlx.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)      certportal.dev.nlx.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)       directory.dev.nlx.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)   directory-api.dev.nlx.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)         insight.dev.nlx.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)            demo.dev.nlx.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)           txlog.dev.rdw.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)        irma-api.dev.rdw.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)     insight-api.dev.rdw.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)           txlog.dev.brp.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)        irma-api.dev.brp.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)     insight-api.dev.brp.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)       txlog.dev.haarlem.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)    irma-api.dev.haarlem.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip) insight-api.dev.haarlem.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip)      outway.dev.haarlem.minikube" | sudo tee -a /etc/hosts
+echo "$(minikube ip) application.dev.haarlem.minikube" | sudo tee -a /etc/hosts
