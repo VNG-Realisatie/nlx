eaef39b50b77e4529f8be57654f2b610e07cd9a9
Niels Dequeker <niels.dequeker@fastmail.com>
2019-02-04 09:49:48 +0100

    #467 Add snapshot test for the ErrorPage

--- directory-ui/src/pages/ErrorPage/ErrorPage.test.jsx
+++ directory-ui/src/pages/ErrorPage/ErrorPage.test.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
+
 import ErrorPage from './ErrorPage'
 
-it('renders without crashing', () => {
-  expect(() => {
-    const div = document.createElement('div')
-    ReactDOM.render(<ErrorPage />, div)
-  }).not.toThrow()
+it('should match the snapshot', () => {
+    expect(shallow(<ErrorPage />)).toMatchSnapshot()
 })
