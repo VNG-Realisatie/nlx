c636bf51d5a7aa6d7d63b02b6b946798f35aa68e
Niels Dequeker <niels.dequeker@fastmail.com>
2019-02-04 09:46:16 +0100

    #467 Refactor App to functional component

--- directory-ui/src/App.jsx
+++ directory-ui/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Navigation from './components/Navigation/Navigation'
 import Directory from './Directory'
 import DocumentationPage from './pages/Documentation/DocumentationPage'
@@ -7,25 +7,20 @@ import './static/css/base-addon.css'
 
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <Router>
-                    <div>
-                        <Navigation />
+const App = () => (
+    <div className="App">
+        <Router>
+            <div>
+                <Navigation />
 
-                        <Route exact path="/"
-                               component={Directory}
-                        />
-                        <Route path="/documentation/:organization_name/:service_name"
-                               component={DocumentationPage}
-                        />
-                    </div>
-                </Router>
+                <Route exact path="/" component={Directory} />
+                <Route
+                    path="/documentation/:organization_name/:service_name"
+                    component={DocumentationPage}
+                />
             </div>
-        )
-    }
-}
+        </Router>
+    </div>
+)
 
 export default App
