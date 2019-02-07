74f797562f82cf4340d74d53044a831a9c6a6868
Niels Dequeker <niels@newfront.eu>
2019-02-07 11:44:36 +0100

    #467 Fix TableHeadCell tests by wrapping element in a valid table structure

--- directory-ui/src/components/Table/TableHeadCell.test.jsx
+++ directory-ui/src/components/Table/TableHeadCell.test.jsx
@@ -1,29 +1,39 @@
 import React from 'react'
-import { shallow, mount } from 'enzyme'
+import { mount } from 'enzyme'
 import TableHeadCell from './TableHeadCell'
 
+const insertTableHeadCellIntoValidTable = tableHeadCell =>
+  <table>
+    <tbody>
+      <tr>
+        { tableHeadCell }
+      </tr>
+    </tbody>
+  </table>
+
 it('should match snapshot', () => {
-  expect(shallow(<TableHeadCell/>)).toMatchSnapshot()
+  const wrapper = mount(insertTableHeadCellIntoValidTable(<TableHeadCell/>))
+  expect(wrapper).toMatchSnapshot()
 })
 
 it('should render the contents', () => {
-  const wrapper = mount(<TableHeadCell>Heading</TableHeadCell>)
+  const wrapper = mount(insertTableHeadCellIntoValidTable(<TableHeadCell>Heading</TableHeadCell>))
   expect(wrapper.text()).toEqual('Heading')
 })
 
 describe('text alignment', () => {
   it('should use left alignment as default', () => {
-    const wrapper = shallow(<tbody><TableHeadCell/></tbody>)
+    const wrapper = mount(insertTableHeadCellIntoValidTable(<TableHeadCell/>))
     expect(wrapper.find(TableHeadCell).prop('align')).toEqual('left')
   })
 
   it('should support center alignment', () => {
-    const wrapper = shallow(<tbody><TableHeadCell align="center"/></tbody>)
+    const wrapper = mount(insertTableHeadCellIntoValidTable(<TableHeadCell align="center"/>))
     expect(wrapper.find(TableHeadCell).prop('align')).toEqual('center')
   })
 
   it('should support right alignment', () => {
-    const wrapper = shallow(<tbody><TableHeadCell align="right"/></tbody>)
+    const wrapper = mount(insertTableHeadCellIntoValidTable(<TableHeadCell align="right"/>))
     expect(wrapper.find(TableHeadCell).prop('align')).toEqual('right')
   })
 })
