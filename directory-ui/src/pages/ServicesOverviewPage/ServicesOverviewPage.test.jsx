4d0a9c3e3abf6da1ba2affdc3469540e47a1a164
Niels Dequeker <niels@newfront.eu>
2019-02-11 17:31:04 +0100

    #467 Fix typo

--- directory-ui/src/pages/ServicesOverviewPage/ServicesOverviewPage.test.jsx
+++ directory-ui/src/pages/ServicesOverviewPage/ServicesOverviewPage.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import DocumentationPage from './ServicesOverviewPage'
+import ServicesOverviewPage from './ServicesOverviewPage'
 
 it('renders without crashing', () => {
   expect(() => {
     const div = document.createElement('div')
-    ReactDOM.render(<DocumentationPage />, div)
+    ReactDOM.render(<ServicesOverviewPage />, div)
   }).not.toThrow()
 })
