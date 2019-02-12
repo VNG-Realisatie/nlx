fe7cd6b9ce9c1cc3ad95d537d794e7a5857b74ae
Niels Dequeker <niels@newfront.eu>
2019-02-12 11:53:13 +0100

    #467 Refactor ServicesTable into a functional component

--- directory-ui/src/components/ServicesTable/ServicesTable.jsx
+++ directory-ui/src/components/ServicesTable/ServicesTable.jsx
@@ -1,78 +1,58 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { func, string, arrayOf, shape } from 'prop-types'
 import { Table, TableBody, TableRow, TableHead, TableHeadCell, SortableTableHeadCell } from "../Table";
 import ServicesTableRow from './ServicesTableRow';
-import { ASCENDING, DESCENDING } from './../Table/SortableTableHeadCell';
 
-const services = [
-  { status: 'online', organization: 'Bitkode', name: 'DemoService', apiType: 'OpenAPI2', apiAddress: 'Icon' },
-  { status: 'online', organization: 'Bitkode', name: 'DemoService', apiType: 'OpenAPI2', apiAddress: 'Icon' },
-  { status: 'offline', organization: 'Bitkode', name: 'DemoService', apiType: 'OpenAPI2', apiAddress: 'Icon' },
-  { status: 'online', organization: 'Bitkode', name: 'DemoService', apiType: 'OpenAPI2', apiAddress: 'Icon' }
-]
+const ServicesTable = ({ services, sortBy, sortOrder, onToggleSorting }) =>
+  <Table>
+    <TableHead>
+      <TableRow>
+        <TableHeadCell style={({ width: '30px' })} align="center" />
+        <SortableTableHeadCell direction={sortBy === 'organization' ? sortOrder : null}
+                               onClick={() => onToggleSorting('organization')}>
+          Organization
+        </SortableTableHeadCell>
+        <SortableTableHeadCell direction={sortBy === 'name' ? sortOrder : null}
+                               onClick={() => onToggleSorting('name')}>
+          Service
+        </SortableTableHeadCell>
+        <TableHeadCell align="right">API type</TableHeadCell>
+        <TableHeadCell/>
+      </TableRow>
+    </TableHead>
 
-class ServicesTable extends Component {
-  constructor(props) {
-    super(props)
+    <TableBody>
+      {
+        services
+          .map((service, i) =>
+            <ServicesTableRow key={i}
+                              status={service.status}
+                              name={service.name}
+                              organization={service.organization}
+                              apiType={service.apiType}
+                              apiAddress={service.apiAddress}
+            />
+          )
+      }
+    </TableBody>
+  </Table>
 
-    this.state = {
-      sortBy: null,
-      sortOrder: null
-    }
+ServicesTable.propTypes = {
+  services: arrayOf(shape({
+    status: string,
+    organization: string,
+    name: string,
+    apiType: string,
+    apiAddress: string
+  })),
+  sortBy: string,
+  sortOrder: string,
+  onToggleSorting: func
+}
 
-    this.toggleSorting = this.toggleSorting.bind(this)
-  }
-
-  toggleSorting(columnName) {
-    const { sortOrder } = this.state
-
-    const property = columnName
-    const direction = sortOrder === null ? ASCENDING :
-      sortOrder === ASCENDING ? DESCENDING : ASCENDING
-
-    this.setState({
-      sortBy: property,
-      sortOrder: direction
-    })
-  }
-
-  render() {
-    const { sortBy, sortOrder } = this.state
-
-    return (
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeadCell style={({width: '30px'})} align="center" />
-            <SortableTableHeadCell direction={sortBy === 'organization' ? sortOrder : null}
-                                   onClick={() => this.toggleSorting('organization')}>
-              Organization
-            </SortableTableHeadCell>
-            <SortableTableHeadCell direction={sortBy === 'name' ? sortOrder : null}
-                                   onClick={() => this.toggleSorting('name')}>
-              Service
-            </SortableTableHeadCell>
-            <TableHeadCell align="right">API type</TableHeadCell>
-            <TableHeadCell/>
-          </TableRow>
-        </TableHead>
-
-        <TableBody>
-          {
-            services
-              .map((service, i) =>
-                <ServicesTableRow key={i}
-                                  status={service.status}
-                                  name={service.name}
-                                  organization={service.organization}
-                                  apiType={service.apiType}
-                                  apiAddress={service.apiAddress}
-                />
-              )
-          }
-        </TableBody>
-      </Table>
-    )
-  }
+ServicesTable.defaultProps = {
+  services: [],
+  onToggleSorting: () => {}
 }
 
 export default ServicesTable
