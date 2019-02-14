f037b52a24c474165f7392b68ce24ffa0fe1ae23
Niels Dequeker <niels@newfront.eu>
2019-02-14 19:20:23 +0100

    #482 Remove superfluous dependencies

--- directory-ui/src/index.js
+++ directory-ui/src/index.js
@@ -3,9 +3,6 @@ import 'react-app-polyfill/ie11'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import 'bootstrap/dist/js/bootstrap.js'
-import 'core-js/fn/string/includes'
-
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
