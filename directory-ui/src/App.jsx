8f0bee1fe69e434de43b0ab653604d951e2b9a3b
Niels Dequeker <niels@newfront.eu>
2019-02-14 14:23:35 +0100

    #482 Remove deprecated styling

--- directory-ui/src/App.jsx
+++ directory-ui/src/App.jsx
@@ -5,8 +5,6 @@ import Navigation from './components/Navigation/Navigation'
 import ServiceOverviewPage from './pages/ServicesOverviewPage/ServicesOverviewPage'
 import DocumentationPage from './pages/DocumentationPage/DocumentationPage'
 
-import './static/css/base-addon.css'
-
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => (
