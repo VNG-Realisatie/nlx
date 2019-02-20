7438888cef126d4dfb9d688421a1f694e8f8b51d
Fabian Buijing <fabianbuijing@gmail.com>
2019-02-20 10:35:37 +0100

    #467 Added .woff for IE + Removed font files that aren't used

--- directory-ui/src/components/GlobalStyles/GlobalStyles.jsx
+++ directory-ui/src/components/GlobalStyles/GlobalStyles.jsx
@@ -6,7 +6,9 @@ const generateFontFaceDefinition = fontDefinition => `
       font-weight: ${fontDefinition.weight}
       font-style: ${fontDefinition.style}
       font-stretch: normal
-      src: url('/public/fonts/source-sans-pro/SourceSansPro-${fontDefinition.fileName}.otf.woff2') format('woff2')
+      src:
+        url('/public/fonts/source-sans-pro/SourceSansPro-${fontDefinition.fileName}.woff2') format('woff2'),
+        url('/public/fonts/source-sans-pro/SourceSansPro-${fontDefinition.fileName}.woff') format('woff')
   }
 `
 
@@ -14,18 +16,9 @@ const generateFontFaceDefinitions = fontDefinitions =>
   fontDefinitions.map(fontDefinition => generateFontFaceDefinition(fontDefinition))
 
 const fontDefinitions = [
-  { weight: 200, style: 'normal', fileName: 'ExtraLight' },
-  { weight: 200, style: 'italic', fileName: 'ExtraLightIt' },
-  { weight: 300, style: 'normal', fileName: 'Light' },
-  { weight: 300, style: 'italic', fileName: 'LightIt' },
   { weight: 400, style: 'normal', fileName: 'Regular' },
-  { weight: 400, style: 'italic', fileName: 'It' },
   { weight: 600, style: 'normal', fileName: 'Semibold' },
-  { weight: 600, style: 'italic', fileName: 'SemiboldIt' },
   { weight: 700, style: 'normal', fileName: 'Bold' },
-  { weight: 700, style: 'italic', fileName: 'BoldIt' },
-  { weight: 900, style: 'normal', fileName: 'Black' },
-  { weight: 900, style: 'italic', fileName: 'BlackIt' },
 ]
 
 export default createGlobalStyle`
@@ -37,7 +30,7 @@ export default createGlobalStyle`
     font-family: 'Source Sans Pro', sans-serif;
     font-size: 14px;
   }
-  
+
   body {
     margin: 0;
   }
