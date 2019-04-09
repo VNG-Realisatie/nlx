215c163aeb0c08045ca77136bab19efe68b01c99
Niels Dequeker <niels@newfront.eu>
2019-04-09 13:42:17 +0200

    Setup initial Redux store #506

--- insight-ui/src/index.js
+++ insight-ui/src/index.js
@@ -6,15 +6,19 @@ import 'react-app-polyfill/ie11'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import store from './store';
 
 import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-    document.getElementById('root'),
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
 )
 
 // If you want your app to work offline and load faster, you can change
