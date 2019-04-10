faccc189814a0c2dc17df10a7028ec9304c00154
Niels Dequeker <niels@newfront.eu>
2019-04-10 09:35:50 +0200

    Remove Service Worker configuration #506

--- insight-ui/src/index.js
+++ insight-ui/src/index.js
@@ -8,7 +8,6 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import store from './store';
 
-import * as serviceWorker from './serviceWorker'
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
@@ -20,8 +19,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister()
