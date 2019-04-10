a3b59a3c30e92206bdfef01d088cd4b2efedc621
Niels Dequeker <niels@newfront.eu>
2019-04-10 10:36:35 +0200

    #506 Add tests for the SidebarContainer + upgrade Enzyme dependencies

--- insight-ui/src/containers/SidebarContainer/index.js
+++ insight-ui/src/containers/SidebarContainer/index.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react'
+import { arrayOf, shape, string } from 'prop-types'
 import {connect} from 'react-redux'
 
 import {fetchOrganizationsRequest } from '../../store/actions'
 import Sidebar from '../../components/Sidebar'
 
-class SidebarContainer extends Component {
+export class SidebarContainer extends Component {
   componentWillMount() {
     this.props.fetchOrganizationsRequest()
   }
+
   render() {
     const { organizations } = this.props
     return <Sidebar organizations={organizations.map(organization => organization.name)} />
   }
+}
+
+SidebarContainer.propTypes = {
+  organizations: arrayOf(shape({
+    name: string.isRequired
+  }))
 }
 
 const mapStateToProps = ({ organizations }) =>
---
+++ insight-ui/src/containers/SidebarContainer/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import { SidebarContainer } from './index'
+
+describe('SidebarContainer', () => {
+  describe('on initialization', () => {
+    it('should fetch the organizations', () => {
+      const props = {
+        organizations: [],
+        fetchOrganizationsRequest: jest.fn()
+      }
+      const wrapper = shallow(<SidebarContainer {...props} />)
+      const instance = wrapper.instance()
+      expect(instance.props.fetchOrganizationsRequest).toHaveBeenCalled()
+    })
+  })
+})
