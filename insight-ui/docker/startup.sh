a1886c7ef77c6e63e253f9ae22d608c7b53ffd4d
Niels Dequeker <niels@newfront.eu>
2019-05-28 13:19:41 +0200

    fix(insight-ui): make sure the runtime environment variables are passed correctly

--- insight-ui/docker/startup.sh
+++ insight-ui/docker/startup.sh
@@ -5,7 +5,7 @@
 # See https://github.com/docker-library/docs/tree/master/nginx#using-environment-variables-in-nginx-configuration
 envsubst < /etc/nginx/conf.d/default.conf.template '$DIRECTORY_INSPECTION_API_URL' > /etc/nginx/conf.d/default.conf
 
-echo "window._env = { \"NAVBAR_HOME_PAGE_URL\": \"${NAVBAR_HOME_PAGE_URL}\" }" > /usr/share/nginx/html/env.js
+echo "window._env = { \"NAVBAR_HOME_PAGE_URL\": \"${NAVBAR_HOME_PAGE_URL}\", \"NAVBAR_ABOUT_PAGE_URL\": \"${NAVBAR_ABOUT_PAGE_URL}\", \"NAVBAR_DOCS_PAGE_URL\": \"${NAVBAR_DOCS_PAGE_URL}\", \"NAVBAR_DIRECTORY_URL\": \"${NAVBAR_DIRECTORY_URL}\" }" > /usr/share/nginx/html/env.js
 
 exec nginx -g 'daemon off;'
 
