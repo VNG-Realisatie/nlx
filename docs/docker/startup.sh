114ac03f0589d45f87f43e4ffc062a78eac54b87
Niels Dequeker <niels@newfront.eu>
2019-06-28 14:50:38 +0200

    style(): update grammar + formatting #540

--- docs/docker/startup.sh
+++ docs/docker/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # the navigation URLs should be configurable at run-time.
-jq -n 'env | {NAVBAR_ABOUT_PAGE_URL, NAVBAR_HOME_PAGE_URL,NAVBAR_DIRECTORY_URL}' > /usr/share/nginx/html/env.json
+jq -n 'env | {NAVBAR_ABOUT_PAGE_URL, NAVBAR_HOME_PAGE_URL, NAVBAR_DIRECTORY_URL}' > /usr/share/nginx/html/env.json
 (echo "window._env = "; cat /usr/share/nginx/html/env.json) > /usr/share/nginx/html/env.js
 rm /usr/share/nginx/html/env.json
 
