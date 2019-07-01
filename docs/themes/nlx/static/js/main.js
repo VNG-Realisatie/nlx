aadea454d9b6475b42a7f33b5b9bb042649eb2f4
Bart Jeukendrup <bart@jeukendrup.nl>
2019-07-01 14:19:28 +0200

    fix(directory-ui): correctly handle empty environment variable

--- docs/themes/nlx/static/js/main.js
+++ docs/themes/nlx/static/js/main.js
@@ -6,16 +6,20 @@ mermaid.initialize({
     startOnLoad:true
 });
 
-if (!window._env) {
-  console.info('#540 The links in the navbar won\'t work during development because we depend on env.js which is generated as part of starting the Nginx container.')
-  return
+function setNavbarLinksBasedOnEnvironment(env) {
+  if (!env) {
+    console.info('#540 The links in the navbar won\'t work during development because we depend on env.js which is generated as part of starting the Nginx container.')
+    return;
+  }
+
+  const navbarLogoLink = document.getElementById('navbar-logo-link');
+  navbarLogoLink.href = env.NAVBAR_HOME_PAGE_URL;
+
+  const headerAboutLink = document.getElementById('navbar-about-link');
+  headerAboutLink.href = env.NAVBAR_ABOUT_PAGE_URL;
+
+  const headerDirectoryLink = document.getElementById('navbar-directory-link');
+  headerDirectoryLink.href = env.NAVBAR_DIRECTORY_URL;
 }
 
-const navbarLogoLink = document.getElementById('navbar-logo-link');
-navbarLogoLink.href = window._env.NAVBAR_HOME_PAGE_URL;
-
-const headerAboutLink = document.getElementById('navbar-about-link');
-headerAboutLink.href = window._env.NAVBAR_ABOUT_PAGE_URL;
-
-const headerDirectoryLink = document.getElementById('navbar-directory-link');
-headerDirectoryLink.href = window._env.NAVBAR_DIRECTORY_URL;
+setNavbarLinksBasedOnEnvironment(window._env)
