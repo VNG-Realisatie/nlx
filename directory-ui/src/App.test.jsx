570947e0ae86cb83655dd67faa62c2b7dd385f78
Bart Jeukendrup <bart@jeukendrup.nl>
2019-09-25 18:27:55 +0200

    chore(directory-ui): update packages and remove snapshot testing

--- directory-ui/src/App.test.jsx
+++ directory-ui/src/App.test.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import App from './App'
 
-it('should match the snapshot', () => {
-    expect(shallow(<App />)).toMatchSnapshot()
+it('exists', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.exists()).toBe(true)
 })
