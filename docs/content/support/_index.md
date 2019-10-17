0a1fd08234f6bea8743c322db3007ffda0b4526c
Niels Dequeker <niels@newfront.eu>
2019-10-17 11:40:10 +0200

    feat(): replace Hugo docs website with basic Docusaurus #760

--- docs/content/support/_index.md
+++
@@ -1,7 +0,0 @@
----
-title: "Support"
-description: ""
----
-
-1. [Contact](./contact/)
-1. [Common errors](./common-errors/)
--- docs/themes/nlx/static/js/main.js
+++
@@ -1,7 +0,0 @@
-// Copyright © VNG Realisatie 2018
-// Licensed under the EUPL
-
-hljs.initHighlightingOnLoad();
-mermaid.initialize({
-    startOnLoad:true
-});
