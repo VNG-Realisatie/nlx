c1cf9739c57c8b8ef332da1c29ca69dc9493f6a7
Niels Dequeker <niels@newfront.eu>
2019-10-21 14:51:30 +0200

    fix(docs): CSP settings #760

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -53,12 +53,10 @@ const siteConfig = {
 
   // Add custom scripts here that would be placed in <script> tags.
   scripts: [
-    'https://buttons.github.io/buttons.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js',
+    '/js/github-buttons-v2.3.6.js',
+    '/js/clipboard-v2.0.0.min.js',
     '/js/code-block-buttons.js',
   ],
-
-  stylesheets: ['/css/code-block-buttons.css'],
 
   // On page navigation for the current documentation page.
   onPageNav: 'separate',
