848fea14c56e57530b1b50965259f41d4c2f3c19
Gilgamesh Nootebos <gilgamez@gmail.com>
2020-01-07 15:34:49 +0100

    chore(management-api): explicit hasing parameters

--- management-api/cmd/hashpasswd/main.go
+++ management-api/cmd/hashpasswd/main.go
@@ -9,6 +9,20 @@ import (
 	"github.com/alexedwards/argon2id"
 )
 
+const hashingMemory = 64 * 1024
+const hashingIterations = 3
+const hashingParallelism = 2
+const hashingSaltLength = 16
+const hashingKeyLength = 32
+
+var hashingParams = &argon2id.Params{
+	Memory:      hashingMemory,
+	Iterations:  hashingIterations,
+	Parallelism: hashingParallelism,
+	SaltLength:  hashingSaltLength,
+	KeyLength:   hashingKeyLength,
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -18,7 +32,7 @@ func main() {
 		// provided algorithm parameters. The returned hash follows the format used
 		// by the Argon2 reference C implementation and looks like this:
 		// $argon2id$v=19$m=65536,t=3,p=2$c29tZXNhbHQ$RdescudvJCsgt3ub+b+dWRWJTmaaJObG
-		hash, err := argon2id.CreateHash(text, argon2id.DefaultParams)
+		hash, err := argon2id.CreateHash(text, hashingParams)
 		if err != nil {
 			log.Fatal(err)
 		}
