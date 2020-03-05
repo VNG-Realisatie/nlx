64730b9f8cfd0c60b310177297721bcd448922ef
Niels Dequeker <niels@newfront.eu>
2020-03-05 15:07:30 +0100

    fix(directory-ui): fix linting #872

--- directory-ui/src/components/Spinner/index.js
+++ directory-ui/src/components/Spinner/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { StyledSpinner, StyledBulletContainer, StyledBullet } from './index.styles';
+import React from 'react'
+import {
+    StyledSpinner,
+    StyledBulletContainer,
+    StyledBullet,
+} from './index.styles'
 
-const Spinner = () =>
-  <StyledSpinner>
-    <StyledBulletContainer>
-      {
-        Array
-          .from({ length: 8 })
-          .map((value, i) => <StyledBullet data-test="bullet" key={i} />)
-      }
-    </StyledBulletContainer>
-  </StyledSpinner>;
+const Spinner = () => (
+    <StyledSpinner>
+        <StyledBulletContainer>
+            {Array.from({ length: 8 }).map((value, i) => (
+                <StyledBullet data-test="bullet" key={i} />
+            ))}
+        </StyledBulletContainer>
+    </StyledSpinner>
+)
 
-export default Spinner;
+export default Spinner
--- directory-ui/src/components/Spinner/index.test.js
+++ directory-ui/src/components/Spinner/index.test.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { shallow } from 'enzyme'
 
 import Spinner from './index'
 
 it('renders without crashing', () => {
-  expect(() => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Spinner />, div)
-  }).not.toThrow()
+    expect(() => {
+        const div = document.createElement('div')
+        ReactDOM.render(<Spinner />, div)
+    }).not.toThrow()
 })
 
 it('should contain 8 dots', () => {
-  const tree = shallow(<Spinner />);
-  expect(tree.find('[data-test="bullet"]').length).toBe(8);
-});
+    const tree = shallow(<Spinner />)
+    expect(tree.find('[data-test="bullet"]')).toHaveLength(8)
+})
