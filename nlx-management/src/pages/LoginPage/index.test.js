632606fc39563342f26b98e28d2f291c3943ba40
Niels Dequeker <niels@newfront.eu>
2020-03-12 17:57:18 +0100

    test(nlx-management): use i18next provider in tests #872

--- nlx-management/src/pages/LoginPage/index.test.js
+++ nlx-management/src/pages/LoginPage/index.test.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { I18nextProvider } from 'react-i18next'
 import { render } from '@testing-library/react'
+import i18n from '../../i18n'
 import LoginPage from './index'
 
 test('renders a welcome message', () => {
-  const { getByText } = render(<LoginPage />)
-  expect(getByText('Welkom')).toBeInTheDocument()
+  const { getByText } = render(
+    <I18nextProvider i18n={i18n}>
+      <LoginPage />
+    </I18nextProvider>,
+  )
+  expect(getByText(/^Welkom$/)).toBeInTheDocument()
 })
