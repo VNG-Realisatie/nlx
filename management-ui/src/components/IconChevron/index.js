ae0bfa104a394d614ddd21b91d90b9187c04ced4
Niels Dequeker <niels@newfront.eu>
2020-03-23 10:31:38 +0100

    refactor(management-ui): replace function body with direct return #876

--- management-ui/src/components/IconChevron/index.js
+++ management-ui/src/components/IconChevron/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const IconChevron = ({ ...props }) => {
-  return (
-    <svg viewBox="0 0 24 24" {...props}>
-      <path d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z" />
-    </svg>
-  )
-}
+const IconChevron = ({ ...props }) => (
+  <svg viewBox="0 0 24 24" {...props}>
+    <path d="M12 13.172l4.95-4.95 1.414 1.414L12 16 5.636 9.636 7.05 8.222z" />
+  </svg>
+)
 
 export default IconChevron
