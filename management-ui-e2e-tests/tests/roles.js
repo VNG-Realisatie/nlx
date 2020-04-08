efdb7f174d5cba039478131ae68218dee4fe158d
Niels Dequeker <niels@newfront.eu>
2020-04-08 22:36:25 +0200

    test(management-ui): update Admin role #896

--- management-ui-e2e-tests/tests/roles.js
+++ management-ui-e2e-tests/tests/roles.js
@@ -1,10 +1,12 @@
 // Copyright © VNG Realisatie 2020
 // Licensed under the EUPL
 
-import { Selector, Role } from 'testcafe'
+import { Selector, Role, ClientFunction } from 'testcafe'
 
 const getBaseUrl = require('../getBaseUrl')
 const baseUrl = getBaseUrl()
+
+const getLocation  = ClientFunction(() => document.location.href.toString());
 
 export const adminUser = Role(`${baseUrl}/login`, async t => {
   const managementLoginButton = Selector('#login');
@@ -19,4 +21,6 @@ export const adminUser = Role(`${baseUrl}/login`, async t => {
     .typeText(dexPasswordText, 'password')
     .click(dexSubmitLoginButton)
     .click(dexGrantAccessButton)
+    .expect(getLocation()).notContains('auth/local')
+    .navigateTo(getBaseUrl())
 });
