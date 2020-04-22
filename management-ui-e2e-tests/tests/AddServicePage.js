6ad2018b9374a7f4e3af233fe1e9d9bd03219a8c
Niels Dequeker <niels@newfront.eu>
2020-04-22 12:12:40 +0200

    test: extract add service into a page object

--- management-ui-e2e-tests/tests/AddServicePage.js
+++ management-ui-e2e-tests/tests/AddServicePage.js
@@ -1,10 +1,10 @@
 // Copyright © VNG Realisatie 2020
 // Licensed under the EUPL
 
-import { Selector } from 'testcafe'
 import { waitForReact } from 'testcafe-react-selectors'
 import { axeCheck, createReport } from 'axe-testcafe'
 import { adminUser } from './roles'
+import page, { AUTHORIZATION_TYPE_NONE } from './page-objects/add-service'
 
 const getBaseUrl = require('../getBaseUrl')
 const baseUrl = getBaseUrl()
@@ -22,31 +22,23 @@ test('Automated accessibility testing', async t => {
 })
 
 test('Page title is visible', async t => {
-  const pageTitle = Selector('h1')
-
   await t
-    .expect(pageTitle.visible).ok()
-    .expect(pageTitle.innerText).eql('Nieuwe service toevoegen')
+    .expect(page.title.visible).ok()
+    .expect(page.title.innerText).eql('Nieuwe service toevoegen')
 })
 
 test('Adding a new service', async t => {
-  const submitButton = Selector('button[type="submit"]')
-  const alert = Selector('[role="alert"]')
-  const nameError = Selector('[data-testid="error-name"]')
+  await page.fillAndSubmitForm({
+    endpointUrl: 'my-service.test:8000',
+    documentationUrl: 'my-service.test:8000/docs',
+    apiSpecificationUrl: 'my-service.test:8000/openapi.json',
+    publishToCentralDirectory: true,
+    techSupportContact: 'tech@organization.test',
+    publicSupportContact: 'public@organization.test',
+    authorizationType: AUTHORIZATION_TYPE_NONE,
+  })
+  await t.expect(page.nameFieldError.innerText).contains('Dit veld is verplicht.')
 
-  await t
-    .typeText('#endpointURL', 'my-service.test:8000')
-    .typeText('#documentationURL', 'my-service.test:8000/docs')
-    .typeText('#apiSpecificationURL', 'my-service.test:8000/openapi.json')
-    .click('#publishedInDirectory')
-    .typeText('#techSupportContact', 'tech@organization.test')
-    .typeText('#publicSupportContact', 'public@organization.test')
-    .click('#authorizationModeNone')
-    .click(submitButton)
-
-    .expect(nameError.innerText).contains('Dit veld is verplicht.')
-    .typeText('#name', 'my-service')
-    .click(submitButton)
-
-    .expect(alert.innerText).contains('De service is toegevoegd.')
+  await page.fillAndSubmitForm({ name: 'my-service' })
+  await t.expect(page.alert.innerText).contains('De service is toegevoegd.')
 })
