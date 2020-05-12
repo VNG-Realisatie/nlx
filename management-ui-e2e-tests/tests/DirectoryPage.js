99c141708026c48797ea10bb8a776f64a7a60d58
Gilgamesh Nootebos <gilgamez@gmail.com>
2020-05-12 20:26:09 +0200

    test(management-ui): e2e test directory page

---
+++ management-ui-e2e-tests/tests/DirectoryPage.js
@@ -0,0 +1,53 @@
+// Copyright © VNG Realisatie 2020
+// Licensed under the EUPL
+
+import { Selector } from 'testcafe'
+import { waitForReact } from 'testcafe-react-selectors'
+import { axeCheck, createReport } from 'axe-testcafe'
+import { adminUser } from './roles'
+import { DIRECTORY_ORGANIZATION_NAME, DIRECTORY_SERVICE_NAME, DIRECTORY_STATUS, DIRECTORY_API_SPECIFICATION_TYPE } from './environment'
+
+const getBaseUrl = require('../getBaseUrl')
+const baseUrl = getBaseUrl()
+
+fixture `Directory page`
+  .beforeEach(async (t) => {
+    await t
+      .useRole(adminUser)
+      .navigateTo(`${baseUrl}/directory`)
+    await waitForReact()
+  })
+
+test('Automated accessibility testing', async t => {
+  const { violations } = await axeCheck(t)
+  await t.expect(violations.length === 0).ok(createReport(violations));
+})
+
+test('Page title is visible', async t => {
+  const pageTitle = Selector('h1')
+
+  await t
+    .expect(pageTitle.visible).ok()
+    .expect(pageTitle.innerText).eql('Directory')
+})
+
+test('Directory details are displayed', async t => {
+  const directoryList = Selector('[data-testid="directory-services"]');
+  const initialService = Selector('tr').withText(DIRECTORY_SERVICE_NAME)
+  const initialServiceColumns = initialService.find('td')
+
+  const organizationNameCell = initialServiceColumns.nth(0)
+  const serviceNameCell = initialServiceColumns.nth(1)
+  const statusTitle = initialServiceColumns.nth(2).find('svg title')
+  const apiSpecificationTypeCell = initialServiceColumns.nth(3)
+
+  await t
+    .expect(directoryList.visible).ok()
+    .expect(directoryList.find('tbody tr').count).gte(2)
+
+    .expect(organizationNameCell.textContent).eql(DIRECTORY_ORGANIZATION_NAME)
+    .expect(serviceNameCell.textContent).eql(DIRECTORY_SERVICE_NAME)
+    .expect(statusTitle.textContent).eql(DIRECTORY_STATUS)
+    .expect(apiSpecificationTypeCell.textContent).eql(DIRECTORY_API_SPECIFICATION_TYPE)
+})
+
