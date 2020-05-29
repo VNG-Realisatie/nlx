edad1cdec0d314a363e21bdbeaf03484cb2c635a
Niels Dequeker <niels@newfront.eu>
2020-05-29 16:48:32 +0200

    test(management-ui): update test to reflect expected behaviour

--- management-ui/src/pages/DirectoryPage/DirectoryServiceStatus/index.test.js
+++ management-ui/src/pages/DirectoryPage/DirectoryServiceStatus/index.test.js
@@ -31,22 +31,12 @@ test('renders an icon', () => {
   expect(container.querySelector('svg')).toHaveAttribute('title', 'Unknown')
 })
 
-test('renders the unknown icon when status is not valid', () => {
-  jest.spyOn(console, 'warn').mockImplementation(() => undefined)
+test('does not render when status is invalid', () => {
   const { container, rerender } = renderWithProviders(
     <DirectoryServiceStatus status={null} />,
   )
   expect(container).toBeEmpty()
-  expect(console.warn).toHaveBeenCalledTimes(1)
-  console.warn.mockRestore()
 
-  jest.spyOn(console, 'warn').mockImplementation(() => undefined)
-  jest.spyOn(console, 'error').mockImplementation(() => undefined)
   rerender(<DirectoryServiceStatus status="invalid" />)
   expect(container).toBeEmpty()
-
-  expect(console.warn).toHaveBeenCalledTimes(1)
-  expect(console.error).toHaveBeenCalledTimes(1)
-  console.warn.mockRestore()
-  console.error.mockRestore()
 })
