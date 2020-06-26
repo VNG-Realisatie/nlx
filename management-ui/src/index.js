02353417358f6fe1f11bdd521145c850513cfdef
Niels Dequeker <niels@newfront.eu>
2020-06-26 18:48:03 +0200

    fix(management-ui): include Object.entries polyfill for IE11
    #942

--- management-ui/src/index.js
+++ management-ui/src/index.js
@@ -4,6 +4,7 @@
 import 'react-app-polyfill/ie11'
 import 'core-js/features/array/find'
 import 'core-js/features/array/includes'
+import 'core-js/features/object/entries'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
