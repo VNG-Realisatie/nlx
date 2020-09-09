5811a71205bebb77f2d18ec70aa8a412751a5e54
Niels Dequeker <niels@newfront.eu>
2020-09-09 10:16:36 +0200

    fix(management): linting
    #1002

--- management-ui/src/routes/index.test.js
+++ management-ui/src/routes/index.test.js
@@ -13,7 +13,9 @@ jest.mock('../pages/LoginPage', () => () => <div data-testid="login-page" />)
 jest.mock('../pages/services/ServicesPage', () => () => (
   <div data-testid="services-page" />
 ))
-jest.mock('../pages/inways/InwaysPage', () => () => <div data-testid="inways-page" />)
+jest.mock('../pages/inways/InwaysPage', () => () => (
+  <div data-testid="inways-page" />
+))
 jest.mock('../pages/services/AddServicePage', () => () => (
   <div data-testid="add-service-page" />
 ))
