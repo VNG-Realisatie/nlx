5f1ec67c88d0bfed45ccc79e3fba73bbd1a8e008
Niels Dequeker <niels@newfront.eu>
2020-09-14 12:21:41 +0200

    docs(insight): fix typo

--- insight-ui/docker/startup.sh
+++ insight-ui/docker/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# The API base URL should be configurable at run-time. Because Nginx does not suppport using environment variables
+# The API base URL should be configurable at run-time. Because Nginx does not support using environment variables
 # in the config file, we use envsubst to substitute the desired value for $DIRECTORY_INSPECTION_API_URL in the config file.
 # See https://github.com/docker-library/docs/tree/master/nginx#using-environment-variables-in-nginx-configuration
 envsubst < /etc/nginx/conf.d/default.conf.template '$DIRECTORY_INSPECTION_API_URL' > /etc/nginx/conf.d/default.conf
