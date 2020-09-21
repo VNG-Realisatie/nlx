6775c622f851c0464ef6b1c48369fad0d4a8abe7
Ronald Koster <ronald.koster07@gmail.com>
2020-09-21 11:40:22 +0200

    feat(management): save organization inway address in the directory

--- management-api/pkg/server/handlers_settings.go
+++ management-api/pkg/server/handlers_settings.go
@@ -11,6 +11,7 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"go.nlx.io/nlx/directory-registration-api/registrationapi"
 	"go.nlx.io/nlx/management-api/api"
 	"go.nlx.io/nlx/management-api/pkg/database"
 )
@@ -33,6 +34,24 @@ func (s *ManagementService) GetSettings(ctx context.Context, _ *types.Empty) (*a
 // UpdateSettings updates the settings for the organization
 func (s *ManagementService) UpdateSettings(ctx context.Context, req *api.UpdateSettingsRequest) (*types.Empty, error) {
 	logger := s.logger.With(zap.String("handler", "update-settings"))
+
+	if req.OrganizationInway != "" {
+		setOrganizationInwayRequest := &registrationapi.SetOrganizationInwayRequest{
+			Address: req.OrganizationInway,
+		}
+
+		_, err := s.directoryClient.SetOrganizationInway(ctx, setOrganizationInwayRequest)
+		if err != nil {
+			logger.Error("could not update the settings in the directory", zap.Error(err))
+			return nil, status.Error(codes.Internal, "database error")
+		}
+	} else {
+		_, err := s.directoryClient.ClearOrganizationInway(ctx, nil)
+		if err != nil {
+			logger.Error("could not clear organization inway in the directory", zap.Error(err))
+			return nil, status.Error(codes.Internal, "database error")
+		}
+	}
 
 	settings := database.Settings{
 		OrganizationInway: req.OrganizationInway,
--- management-api/pkg/server/handlers_settings_test.go
+++ management-api/pkg/server/handlers_settings_test.go
@@ -16,9 +16,11 @@ import (
 	"google.golang.org/grpc/status"
 
 	"go.nlx.io/nlx/common/process"
+	"go.nlx.io/nlx/directory-registration-api/registrationapi"
 	"go.nlx.io/nlx/management-api/api"
 	"go.nlx.io/nlx/management-api/pkg/database"
 	mock_database "go.nlx.io/nlx/management-api/pkg/database/mock"
+	"go.nlx.io/nlx/management-api/pkg/directory"
 	mock_directory "go.nlx.io/nlx/management-api/pkg/directory/mock"
 	"go.nlx.io/nlx/management-api/pkg/server"
 )
@@ -38,6 +40,7 @@ func TestManagementService_GetSettings(t *testing.T) {
 
 				return db
 			},
+
 			expectedResponse: nil,
 			expectedError:    status.Error(codes.Internal, "database error"),
 		},
@@ -51,6 +54,7 @@ func TestManagementService_GetSettings(t *testing.T) {
 
 				return db
 			},
+
 			expectedResponse: &api.Settings{
 				OrganizationInway: "inway-name",
 			},
@@ -77,14 +81,56 @@ func TestManagementService_GetSettings(t *testing.T) {
 	}
 }
 
+//nolint:funlen alot of scenario's to test
 func TestManagementService_UpdateSettings(t *testing.T) {
 	tests := []struct {
 		name             string
 		db               func(ctrl *gomock.Controller) database.ConfigDatabase
+		directoryClient  func(ctrl *gomock.Controller) directory.Client
 		req              *api.UpdateSettingsRequest
 		expectedResponse *types.Empty
 		expectedError    error
 	}{
+		{
+			name: "when the directory call fails",
+			db: func(ctrl *gomock.Controller) database.ConfigDatabase {
+				db := mock_database.NewMockConfigDatabase(ctrl)
+
+				return db
+			},
+			directoryClient: func(ctrl *gomock.Controller) directory.Client {
+				directoryClient := mock_directory.NewMockClient(ctrl)
+				directoryClient.EXPECT().SetOrganizationInway(gomock.Any(), &registrationapi.SetOrganizationInwayRequest{
+					Address: "inway-name",
+				}).Return(nil, errors.New("arbitrary error"))
+
+				return directoryClient
+			},
+			req: &api.UpdateSettingsRequest{
+				OrganizationInway: "inway-name",
+			},
+			expectedResponse: nil,
+			expectedError:    status.Error(codes.Internal, "database error"),
+		},
+		{
+			name: "when the inway is empty and the directory call fails",
+			db: func(ctrl *gomock.Controller) database.ConfigDatabase {
+				db := mock_database.NewMockConfigDatabase(ctrl)
+
+				return db
+			},
+			directoryClient: func(ctrl *gomock.Controller) directory.Client {
+				directoryClient := mock_directory.NewMockClient(ctrl)
+				directoryClient.EXPECT().ClearOrganizationInway(gomock.Any(), gomock.Any()).Return(nil, errors.New("arbitrary error"))
+
+				return directoryClient
+			},
+			req: &api.UpdateSettingsRequest{
+				OrganizationInway: "",
+			},
+			expectedResponse: nil,
+			expectedError:    status.Error(codes.Internal, "database error"),
+		},
 		{
 			name: "when the database call fails",
 			db: func(ctrl *gomock.Controller) database.ConfigDatabase {
@@ -94,6 +140,14 @@ func TestManagementService_UpdateSettings(t *testing.T) {
 				).Return(errors.New("arbitrary error"))
 
 				return db
+			},
+			directoryClient: func(ctrl *gomock.Controller) directory.Client {
+				directoryClient := mock_directory.NewMockClient(ctrl)
+				directoryClient.EXPECT().SetOrganizationInway(gomock.Any(), &registrationapi.SetOrganizationInwayRequest{
+					Address: "inway-name",
+				}).Return(&types.Empty{}, nil)
+
+				return directoryClient
 			},
 			req: &api.UpdateSettingsRequest{
 				OrganizationInway: "inway-name",
@@ -111,8 +165,37 @@ func TestManagementService_UpdateSettings(t *testing.T) {
 
 				return db
 			},
+			directoryClient: func(ctrl *gomock.Controller) directory.Client {
+				directoryClient := mock_directory.NewMockClient(ctrl)
+				directoryClient.EXPECT().SetOrganizationInway(gomock.Any(), &registrationapi.SetOrganizationInwayRequest{
+					Address: "inway-name",
+				}).Return(&types.Empty{}, nil)
+
+				return directoryClient
+			},
 			req: &api.UpdateSettingsRequest{
 				OrganizationInway: "inway-name",
+			},
+			expectedResponse: &types.Empty{},
+			expectedError:    nil,
+		},
+		{
+			name: "happy flow with empty inway name",
+			db: func(ctrl *gomock.Controller) database.ConfigDatabase {
+				db := mock_database.NewMockConfigDatabase(ctrl)
+				db.EXPECT().UpdateSettings(gomock.Any(), &database.Settings{
+					OrganizationInway: "",
+				}).Return(nil)
+
+				return db
+			},
+			directoryClient: func(ctrl *gomock.Controller) directory.Client {
+				directoryClient := mock_directory.NewMockClient(ctrl)
+				directoryClient.EXPECT().ClearOrganizationInway(gomock.Any(), gomock.Any()).Return(&types.Empty{}, nil)
+				return directoryClient
+			},
+			req: &api.UpdateSettingsRequest{
+				OrganizationInway: "",
 			},
 			expectedResponse: &types.Empty{},
 			expectedError:    nil,
@@ -127,9 +210,8 @@ func TestManagementService_UpdateSettings(t *testing.T) {
 
 			l := zap.NewNop()
 			p := process.NewProcess(l)
-			d := mock_directory.NewMockClient(ctrl)
 
-			h := server.NewManagementService(l, p, d, tt.db(ctrl))
+			h := server.NewManagementService(l, p, tt.directoryClient(ctrl), tt.db(ctrl))
 			got, err := h.UpdateSettings(context.Background(), tt.req)
 
 			assert.Equal(t, tt.expectedResponse, got)
