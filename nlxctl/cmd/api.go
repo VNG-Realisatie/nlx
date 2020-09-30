6a603bb24ff701645facb4f7bf3f3c62dcf0d2f3
Hugo Stijns <h.stijns@developers.nl>
2020-09-30 09:52:06 +0200

    fix(management): gosec G402 TLS MinVersion

--- nlxctl/cmd/api.go
+++ nlxctl/cmd/api.go
@@ -1,32 +1,23 @@
 package cmd
 
 import (
-	"crypto/tls"
 	"log"
 
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
-	"go.nlx.io/nlx/common/orgtls"
+	common_tls "go.nlx.io/nlx/common/tls"
 	"go.nlx.io/nlx/management-api/api"
 )
 
 func getManagementClient() api.ManagementClient {
-	ca, err := orgtls.LoadRootCert(viper.GetString("ca-path"))
+	certificate, err := common_tls.NewBundleFromFiles(viper.GetString("cert-path"), viper.GetString("key-path"), viper.GetString("ca-path"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	keyPair, err := tls.LoadX509KeyPair(viper.GetString("cert-path"), viper.GetString("key-path"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	creds := credentials.NewTLS(&tls.Config{
-		Certificates: []tls.Certificate{keyPair},
-		RootCAs:      ca,
-	})
+	creds := credentials.NewTLS(certificate.TLSConfig())
 
 	c, err := grpc.Dial(viper.GetString("api-address"), grpc.WithTransportCredentials(creds))
 	if err != nil {
