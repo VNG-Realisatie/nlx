df1e02b985023c163c9eb0b9629d8c8a6468bf2c
Niels Dequeker <niels@newfront.eu>
2020-10-13 14:18:13 +0200

    style(management): remove superfluous comment
    Issue: 1020

--- management-ui/src/pages/services/ServiceDetailPage/ServiceDetailView/AccessRequestsSection/IncomingAccessRequestRow/index.styles.js
+++ management-ui/src/pages/services/ServiceDetailPage/ServiceDetailView/AccessRequestsSection/IncomingAccessRequestRow/index.styles.js
@@ -4,7 +4,6 @@
 import styled from 'styled-components'
 import { Table } from '@commonground/design-system'
 
-// Width set to make sure it stays the same no matter the displayed text
 export const TdActions = styled(Table.Td)`
   width: 140px;
 `
