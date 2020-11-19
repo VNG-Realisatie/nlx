4a58e26cb87fb853d03551084c08904a8c2b51b0
Niels Dequeker <niels@newfront.eu>
2020-11-19 10:29:19 +0100

    test(management): cleanup tests for the InwayDetailPage
    Issue: #1007

--- management-ui/src/pages/inways/InwayDetailPage/index.test.js
+++ management-ui/src/pages/inways/InwayDetailPage/index.test.js
@@ -2,12 +2,8 @@
 // Licensed under the EUPL
 //
 import React from 'react'
-import { StaticRouter as Router, Route } from 'react-router-dom'
-
+import { Route, StaticRouter as Router } from 'react-router-dom'
 import { renderWithProviders } from '../../../test-utils'
-import { mockServicesStore } from '../../../stores/ServicesStore.mock'
-import { mockInwaysStore } from '../../../stores/InwaysStore.mock'
-import { StoreProvider } from '../../../stores'
 import InwayDetailPage from './index'
 
 /* eslint-disable react/prop-types */
@@ -17,13 +13,10 @@ jest.mock('./InwayDetailPageView', () => ({ inway }) => (
 /* eslint-enable react/prop-types */
 
 test('display inway details', () => {
-  const store = mockInwaysStore({})
   const { getByTestId } = renderWithProviders(
     <Router location="/inways/forty-two">
       <Route path="/inways/:name">
-        <StoreProvider store={store}>
-          <InwayDetailPage inway={{ name: 'forty-two' }} />
-        </StoreProvider>
+        <InwayDetailPage inway={{ name: 'forty-two' }} />
       </Route>
     </Router>,
   )
@@ -31,37 +24,11 @@ test('display inway details', () => {
   expect(getByTestId('inway-details')).toHaveTextContent('forty-two')
 })
 
-test('fetching a non-existing component', async () => {
-  const selectInway = jest.fn()
-  const store = mockInwaysStore({ selectInway })
-
+test('display a non-existing inway', async () => {
   const { findByTestId } = renderWithProviders(
     <Router location="/inways/forty-two">
       <Route path="/inways/:name">
-        <StoreProvider store={store}>
-          <InwayDetailPage />
-        </StoreProvider>
-      </Route>
-    </Router>,
-  )
-
-  const message = await findByTestId('error-message')
-  expect(message).toBeTruthy()
-  expect(message.textContent).toBe('Failed to load the details for this inway')
-
-  const closeButton = await findByTestId('close-button')
-  expect(closeButton).toBeTruthy()
-})
-
-test('fetching inway details fails for an unknown reason', async () => {
-  const store = mockServicesStore({ error: 'arbitrary reason' })
-
-  const { findByTestId } = renderWithProviders(
-    <Router location="/inways/42">
-      <Route path="/inways/:name">
-        <StoreProvider store={store}>
-          <InwayDetailPage />
-        </StoreProvider>
+        <InwayDetailPage inway={null} />
       </Route>
     </Router>,
   )
