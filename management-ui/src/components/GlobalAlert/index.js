b1cfe8b2328843ea83644e3b5d6c60f6180b01f4
Jorn Luiten <contact@frontrain.net>
2020-12-17 09:26:55 +0100

    fix(management): a11y warnings

--- management-ui/src/components/GlobalAlert/index.js
+++ management-ui/src/components/GlobalAlert/index.js
@@ -7,7 +7,7 @@ import { IconWarning } from '../../icons'
 import { Alert, StyledIcon } from './index.styles'
 
 const GlobalAlert = ({ children }) => (
-  <Alert>
+  <Alert role="alert">
     <StyledIcon as={IconWarning} inline />
     {children}
   </Alert>
