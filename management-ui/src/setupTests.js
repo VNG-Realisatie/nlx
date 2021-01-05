97255c3fca1d9cd6f1aa3c03ada3c575d6054130
Niels Dequeker <niels@newfront.eu>
2021-01-05 10:28:09 +0100

    test(management): simplify test setup for Jest v26
    CRA 4.x now includes Jest v26, which allows us to get rid of
    @sheerun/mutationobserver-shim
    
    Issue: #1131

--- management-ui/src/setupTests.js
+++ management-ui/src/setupTests.js
@@ -4,11 +4,6 @@
 import '@testing-library/jest-dom/extend-expect'
 import { configure } from 'mobx'
 
-// the MutationObserver shim is added because CRA does not yet support Jest v25.
-// open issue: https://github.com/facebook/create-react-app/pull/8362
-import MutationObserver from '@sheerun/mutationobserver-shim'
-window.MutationObserver = MutationObserver
-
 configure({ enforceActions: 'never' })
 
 // Prevent fetch from going out to the network during test
