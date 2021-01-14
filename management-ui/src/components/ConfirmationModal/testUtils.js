126d4944730ff7bb991eef357995718c922bff54
Jorn Luiten <contact@frontrain.net>
2021-01-14 10:02:59 +0100

    test(management): add testutils to confirmationModal

---
+++ management-ui/src/components/ConfirmationModal/testUtils.js
@@ -0,0 +1,17 @@
+// Copyright © VNG Realisatie 2020
+// Licensed under the EUPL
+//
+import { fireEvent, waitFor } from '@testing-library/react'
+
+export function clickConfirmButton(button) {
+  jest.useFakeTimers()
+  fireEvent.click(button)
+  jest.runAllTimers()
+  jest.runOnlyPendingTimers()
+  jest.useRealTimers()
+}
+
+export function clickConfirmButtonAndAssert(button, assertion) {
+  clickConfirmButton(button)
+  return waitFor(assertion)
+}
