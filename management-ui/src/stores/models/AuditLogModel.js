0a06fa0ee9336bb4ab8113e7aefeb8ce3bcba152
Niels Dequeker <niels@newfront.eu>
2021-02-16 10:44:56 +0100

    fix(management): sync AuditLog action types of client with API
    Issue: #1016

--- management-ui/src/stores/models/AuditLogModel.js
+++ management-ui/src/stores/models/AuditLogModel.js
@@ -5,20 +5,20 @@ import { makeAutoObservable } from 'mobx'
 
 export const ACTION_LOGIN_SUCCESS = 'loginSuccess'
 export const ACTION_LOGIN_FAIL = 'loginFail'
-export const ACTION_LOGOUT_SUCCESS = 'logoutSuccess'
+export const ACTION_LOGOUT_SUCCESS = 'logout'
 export const ACTION_INCOMING_ACCESS_REQUEST_ACCEPT =
   'incomingAccessRequestAccept'
 export const ACTION_INCOMING_ACCESS_REQUEST_REJECT =
   'incomingAccessRequestReject'
-export const ACTION_ACCESS_GRANT_REVOKE = 'access_grant_revoke'
+export const ACTION_ACCESS_GRANT_REVOKE = 'accessGrantRevoke'
 export const ACTION_OUTGOING_ACCESS_REQUEST_CREATE =
   'outgoingAccessRequestCreate'
 export const ACTION_SERVICE_CREATE = 'serviceCreate'
 export const ACTION_SERVICE_UPDATE = 'serviceUpdate'
 export const ACTION_SERVICE_DELETE = 'serviceDelete'
-export const ACTION_ORGANIZATION_SETTINGS_UPDATE = 'organizationSettingsUpdate'
+export const ACTION_ORGANIZATION_SETTINGS_UPDATE = 'organizationSetttingsUpdate'
 export const ACTION_INSIGHT_CONFIGURATION_UPDATE =
-  'organizationInsightConfiguration_update'
+  'organizationInsightConfigurationUpdate'
 
 class AuditLogModel {
   id = null
