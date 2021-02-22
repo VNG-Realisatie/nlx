a09252237fe92a424618d026f2b23b9f60b2450f
Niels Dequeker <niels@newfront.eu>
2021-02-22 14:47:50 +0100

    test(management): test adding service

--- management-ui-e2e-tests-cypress/cypress/integration/services.js
+++ management-ui-e2e-tests-cypress/cypress/integration/services.js
@@ -4,6 +4,19 @@
 
 import logViolations from '../axe-utilities/log-violations'
 
+function randomNumberString() {
+  return `${Math.floor(Math.random() * 10000)}`
+}
+
+function getTestName() {
+  const testName = Cypress.mocha.getRunner().suite.ctx.test.title
+  return testName.replaceAll(' ', '-')
+}
+
+function generateServiceName() {
+  return `service-e2e-${getTestName()}-${randomNumberString()}`
+}
+
 describe('Services', () => {
   beforeEach(() => {
     cy.loginUsingDex()
@@ -14,5 +27,26 @@ describe('Services', () => {
 
   it('Overview list is accessible', () => {
     cy.checkA11y(null, null, logViolations)
+  })
+
+  it('Add and remove service', () => {
+    const serviceName = generateServiceName()
+
+    cy.visit('/services/add-service')
+    cy.findByLabelText('Servicenaam').type(serviceName)
+    cy.findByLabelText('API endpoint URL').type('my-service.test:8000')
+    cy.findByLabelText('API documentatie URL').type('my-service.test:8000/docs')
+    cy.findByLabelText('API specificatie URL').type(
+      'my-service.test:8000/openapi.json',
+    )
+    cy.findByLabelText('Technisch support e-mailadres').type(
+      'tech@organization.test',
+    )
+    cy.findByLabelText('Publiek support e-mailadres').type(
+      'public@organization.test',
+    )
+    cy.findByLabelText('Publiceren in de centrale directory').uncheck()
+
+    cy.findByText('Service toevoegen').click()
   })
 })
