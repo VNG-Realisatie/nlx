bf18b6c4afd178e97cf955c6c8eb9d7b465cfbf8
Jorn Luiten <contact@frontrain.net>
2021-04-21 08:43:24 +0200

    fix(management): dayjs locale loading order

--- management-ui/src/i18n.js
+++ management-ui/src/i18n.js
@@ -2,14 +2,14 @@
 // Licensed under the EUPL
 //
 import i18n from 'i18next'
-import XHR from 'i18next-xhr-backend'
+import HttpApi from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 import dayjs from 'dayjs'
 
 import options from './i18n.options'
 
 i18n
-  .use(XHR)
+  .use(HttpApi)
   .use(initReactI18next) // passes i18n down to react-i18next
   .on('languageChanged', async (lng) => {
     try {
