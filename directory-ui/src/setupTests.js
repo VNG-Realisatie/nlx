9436b1d2c05c1a3026ee6d18430d31a177c3a67a
Niels Dequeker <niels@newfront.eu>
2021-06-02 16:25:37 +0200

    build(directory): remove all references to Enzyme
    Enzyme has been replaced by React Testing Library

--- directory-ui/src/setupTests.js
+++
@@ -1,8 +0,0 @@
-// Copyright © VNG Realisatie 2020
-// Licensed under the EUPL
-//
-
-import { configure } from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-
-configure({ adapter: new Adapter() })
