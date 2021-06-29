3c7806b2e46a1fce68361a29a6c0416df929957c
Dillen Meijboom <info@excol.nl>
2021-06-29 15:26:06 +0200

    fix: rename incoming order fields

--- management-api/db/migrations/011_add_incoming_orders.up.sql
+++ management-api/db/migrations/011_add_incoming_orders.up.sql
@@ -7,14 +7,14 @@ BEGIN transaction;
 
 CREATE TABLE nlx_management.incoming_orders
 (
-    order_id        BIGSERIAL PRIMARY KEY,
-    description     VARCHAR(100)             NOT NULL,
-    public_key_pem  VARCHAR(4096)            NOT NULL,
-    delegator       VARCHAR(100)             NOT NULL,
-    reference       VARCHAR(100)             NOT NULL,
-    valid_from      timestamp with time zone NOT NULL,
-    valid_until     timestamp with time zone NOT NULL,
-    created_at      timestamp with time zone NOT NULL
+    id             BIGSERIAL PRIMARY KEY,
+    description    VARCHAR(100)             NOT NULL,
+    public_key_pem VARCHAR(4096)            NOT NULL,
+    delegator      VARCHAR(100)             NOT NULL,
+    reference      VARCHAR(100)             NOT NULL,
+    valid_from     timestamp with time zone NOT NULL,
+    valid_until    timestamp with time zone NOT NULL,
+    created_at     timestamp with time zone NOT NULL
 );
 
 CREATE INDEX idx_incoming_orders_delegator ON nlx_management.incoming_orders (delegator);
@@ -22,18 +22,18 @@ CREATE UNIQUE INDEX idx_incoming_orders_reference ON nlx_management.incoming_ord
 
 CREATE TABLE nlx_management.incoming_orders_services
 (
-    order_id     BIGSERIAL    NOT NULL,
-    service      VARCHAR(100) NOT NULL,
-    organization VARCHAR(100) NOT NULL,
+    incoming_order_id BIGSERIAL    NOT NULL,
+    service           VARCHAR(100) NOT NULL,
+    organization      VARCHAR(100) NOT NULL,
 
     CONSTRAINT fk_order
-        FOREIGN KEY (order_id)
-            REFERENCES nlx_management.incoming_orders (order_id)
+        FOREIGN KEY (incoming_order_id)
+            REFERENCES nlx_management.incoming_orders (id)
             ON DELETE RESTRICT
 );
 
 CREATE INDEX idx_incoming_orders_service_name ON nlx_management.incoming_orders_services (service);
 CREATE INDEX idx_incoming_orders_service_organization_name ON nlx_management.incoming_orders_services (organization);
-CREATE UNIQUE INDEX idx_incoming_orders_services ON nlx_management.incoming_orders_services (order_id, service, organization);
+CREATE UNIQUE INDEX idx_incoming_orders_services ON nlx_management.incoming_orders_services (incoming_order_id, service, organization);
 
 COMMIT;
