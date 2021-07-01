ee8cc68d6f929c637a5f976ad6352b0522023217
Jorn Luiten <contact@frontrain.net>
2021-07-01 14:09:36 +0200

    fix: revert easter eggs
    remove easter eggs from nav bar and user menu :)

--- management-ui/src/components/Avatar/index.js
+++ management-ui/src/components/Avatar/index.js
@@ -2,21 +2,19 @@
 // Licensed under the EUPL
 //
 import React from 'react'
-import { string, bool } from 'prop-types'
+import { string } from 'prop-types'
 import { useTranslation } from 'react-i18next'
 
 import DefaultAvatar from './default-avatar.svg'
-import DefaultAvatar2 from './default-avatar-p.svg'
 import { Figure } from './index.styles'
 
-const Avatar = ({ url, alt, menuIsOpen, ...props }) => {
+const Avatar = ({ url, alt, ...props }) => {
   const { t } = useTranslation()
-  const TheAvatar = menuIsOpen ? DefaultAvatar2 : DefaultAvatar
   return (
     <Figure {...props}>
       <img
         className="avatar-image"
-        src={url || TheAvatar}
+        src={url || DefaultAvatar}
         alt={alt || t('Avatar')}
       />
     </Figure>
@@ -26,7 +24,6 @@ const Avatar = ({ url, alt, menuIsOpen, ...props }) => {
 Avatar.propTypes = {
   url: string,
   alt: string,
-  menuIsOpen: bool,
 }
 
 export default Avatar
--- management-ui/src/components/PrimaryNavigation/index.js
+++ management-ui/src/components/PrimaryNavigation/index.js
@@ -6,23 +6,17 @@ import { useTranslation } from 'react-i18next'
 import {
   IconArrowLeftRight,
   IconDirectory,
+  IconServices,
+  IconBarChart,
   IconTimeLine,
   IconUserCheck,
   IconSettings,
 } from '../../icons'
-import { ReactComponent as IconServices } from '../../icons/services2.svg'
 import {
   Nav,
   StyledHomeLink,
   StyledLink,
   StyledIcon,
-  InwaysIcon,
-  ServicesIcon,
-  DirectoryIcon,
-  BarChartIcon,
-  TimeIcon,
-  OrdersLink,
-  SettingsIcon,
   StyledNLXManagementLogo,
 } from './index.styles'
 
@@ -41,42 +35,38 @@ const PrimaryNavigation = () => {
         </StyledHomeLink>
 
         <StyledLink to="/inways" aria-label={t('Inways page')}>
-          <InwaysIcon as={IconArrowLeftRight} size="x-large" />
+          <StyledIcon as={IconArrowLeftRight} size="x-large" />
           {t('Inways')}
         </StyledLink>
 
         <StyledLink to="/services" aria-label={t('Services page')}>
-          <ServicesIcon as={IconServices} size="x-large" />
+          <StyledIcon as={IconServices} size="x-large" />
           {t('Services')}
         </StyledLink>
 
         <StyledLink to="/directory" aria-label={t('Directory page')}>
-          <DirectoryIcon as={IconDirectory} size="x-large" />
+          <StyledIcon as={IconDirectory} size="x-large" />
           {t('Directory')}
         </StyledLink>
 
         <StyledLink to="/finances" aria-label={t('Finances page')}>
-          <BarChartIcon>
-            <div />
-            <div />
-            <div />
-          </BarChartIcon>
+          <StyledIcon as={IconBarChart} size="x-large" />
           {t('Finances')}
         </StyledLink>
 
         <StyledLink to="/audit-log" aria-label={t('Audit log page')}>
-          <TimeIcon as={IconTimeLine} size="x-large" />
+          <StyledIcon as={IconTimeLine} size="x-large" />
           {t('Logs')}
         </StyledLink>
       </section>
 
       <section>
-        <OrdersLink to="/orders" aria-label={t('Orders page')}>
+        <StyledLink to="/orders" aria-label={t('Orders page')}>
           <StyledIcon as={IconUserCheck} size="x-large" />
           {t('Orders')}
-        </OrdersLink>
+        </StyledLink>
         <StyledLink to="/settings" aria-label={t('Settings page')}>
-          <SettingsIcon as={IconSettings} size="x-large" />
+          <StyledIcon as={IconSettings} size="x-large" />
           {t('Settings')}
         </StyledLink>
       </section>
