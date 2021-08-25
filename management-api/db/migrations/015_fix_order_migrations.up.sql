0175939e6d42b7fd5358c1e01726221d4cba1b4b
Niels Dequeker <niels@newfront.eu>
2021-08-25 12:09:12 +0200

    feat(management): enforce outgoing orders have a unique combination of delegatee and reference
    Issue: #1181

---
+++ management-api/db/migrations/015_fix_order_migrations.up.sql
@@ -0,0 +1,6 @@
+begin;
+
+DROP INDEX nlx_management.idx_outgoing_orders_reference;
+CREATE UNIQUE INDEX idx_outgoing_orders_delegatee_reference ON nlx_management.outgoing_orders (delegatee, reference);
+
+commit;
