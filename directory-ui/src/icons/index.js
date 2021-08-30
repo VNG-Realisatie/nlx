5cacca2bfd5c9a34b6ac8bd6d49c6f089e60a871
Niels Dequeker <niels@newfront.eu>
2021-08-30 14:28:21 +0200

    refactor(directory): remove unused icons

--- directory-ui/src/icons/index.js
+++ directory-ui/src/icons/index.js
@@ -4,57 +4,25 @@
 import React from 'react'
 import { Icon } from '@commonground/design-system'
 
-import { ReactComponent as IconBox } from './box.svg'
-import { ReactComponent as IconBuilding } from './building.svg'
-import { ReactComponent as IconCheck } from './check.svg'
 import { ReactComponent as IconChevronRight } from './chevron-right.svg'
 import { ReactComponent as IconExternalLink } from './external-link.svg'
-import { ReactComponent as IconFileCopy } from './file.svg'
-import { ReactComponent as IconGitlab } from './gitlab.svg'
-import { ReactComponent as IconGroup } from './group-2.svg'
-import { ReactComponent as IconHammer } from './hammer.svg'
 import { ReactComponent as IconHome } from './home.svg'
-import { ReactComponent as IconInfo } from './info.svg'
-import { ReactComponent as IconLightning } from './flashlight.svg'
 import { ReactComponent as IconMail } from './mail.svg'
-import { ReactComponent as IconOpenArm } from './open-arm.svg'
-import { ReactComponent as IconPlug } from './plug.svg'
-import { ReactComponent as IconRecycle } from './recycle.svg'
-import { ReactComponent as IconSearch } from './search.svg'
-import { ReactComponent as IconShieldCheck } from './shield-check.svg'
-import { ReactComponent as IconSpy } from './spy.svg'
 import { ReactComponent as IconStateDegraded } from './state-degraded.svg'
 import { ReactComponent as IconStateDown } from './state-down.svg'
 import { ReactComponent as IconStateUnknown } from './state-unknown.svg'
 import { ReactComponent as IconStateUp } from './state-up.svg'
-import { ReactComponent as IconTools } from './tools.svg'
 import { ReactComponent as MoneyEuroCircleLine } from './money-euro-circle-line.svg'
 
 export {
-  IconBox,
-  IconBuilding,
-  IconCheck,
   IconChevronRight,
   IconExternalLink,
-  IconFileCopy,
-  IconGitlab,
-  IconGroup,
-  IconHammer,
   IconHome,
-  IconInfo,
-  IconLightning,
   IconMail,
-  IconOpenArm,
-  IconPlug,
-  IconRecycle,
-  IconSearch,
-  IconShieldCheck,
-  IconSpy,
   IconStateDegraded,
   IconStateDown,
   IconStateUnknown,
   IconStateUp,
-  IconTools,
 }
 
 export const IconMoneyEuroCircleLine = (props) => (
