7c5abbc879c64f87095a0e394faea4ff25bc0efe
Niels Dequeker <niels@newfront.eu>
2021-08-30 15:25:25 +0200

    refactor(directory): rework components to get rid of the debounce dependency

--- directory-ui/src/pages/services/ServicesPage/index.js
+++ directory-ui/src/pages/services/ServicesPage/index.js
@@ -3,7 +3,7 @@
 //
 
 import React, { useEffect, useState } from 'react'
-import debounce from 'debounce'
+import debounce from '@commonground/design-system/dist/utils/debounce'
 import { object } from 'prop-types'
 import { Route, useParams } from 'react-router-dom'
 import Spinner from '../../../components/Spinner'
