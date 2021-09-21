84bd6fc15c19e12b48223d277552c5408534c246
Niels Dequeker <niels@newfront.eu>
2021-09-21 15:08:28 +0200

    test(management): add integration test for listing incoming Access Requests
    Issue: #1367

---
+++ management-api/pkg/database/access_request_list_all_incoming_test.go
@@ -0,0 +1,90 @@
+// Copyright © VNG Realisatie 2021
+// Licensed under the EUPL
+
+//go:build integration
+// +build integration
+
+package database_test
+
+import (
+	"context"
+	"testing"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/require"
+
+	"go.nlx.io/nlx/management-api/pkg/database"
+)
+
+func TestListAllIncomingAccessRequests(t *testing.T) {
+	t.Parallel()
+
+	setup(t)
+
+	fixtureTime := getFixtureTime(t)
+
+	fixtureCertBundle, err := newFixtureCertificateBundle()
+	require.NoError(t, err)
+
+	fixturePublicKeyPEM, err := fixtureCertBundle.PublicKeyPEM()
+	require.NoError(t, err)
+
+	tests := map[string]struct {
+		loadFixtures bool
+		want         []*database.IncomingAccessRequest
+		wantErr      error
+	}{
+		"happy_flow": {
+			loadFixtures: true,
+			want: []*database.IncomingAccessRequest{
+				{
+					ID:        1,
+					ServiceID: 1,
+					Service: &database.Service{
+						ID:                     1,
+						Name:                   "fixture-service-name",
+						EndpointURL:            "http://fixture-api:8000",
+						DocumentationURL:       "https://fixture-documentation-url.com",
+						APISpecificationURL:    "http://fixture-api:8000/schema?format=openapi-json",
+						Internal:               false,
+						TechSupportContact:     "fixture@tech-support-contact.com",
+						PublicSupportContact:   "fixture@public-support-contact.com",
+						Inways:                 nil,
+						IncomingAccessRequests: nil,
+						OneTimeCosts:           1,
+						MonthlyCosts:           2,
+						RequestCosts:           3,
+						CreatedAt:              fixtureTime,
+						UpdatedAt:              fixtureTime,
+					},
+					OrganizationName:     "fixture-organization-name",
+					State:                database.IncomingAccessRequestReceived,
+					CreatedAt:            fixtureTime,
+					UpdatedAt:            fixtureTime,
+					PublicKeyPEM:         fixturePublicKeyPEM,
+					PublicKeyFingerprint: fixtureCertBundle.PublicKeyFingerprint(),
+				},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for name, tt := range tests {
+		tt := tt
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			configDb, close := newConfigDatabase(t, t.Name(), tt.loadFixtures)
+			defer close()
+
+			got, err := configDb.ListAllIncomingAccessRequests(context.Background())
+			require.ErrorIs(t, err, tt.wantErr)
+
+			if tt.wantErr == nil {
+				require.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
