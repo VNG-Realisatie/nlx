0bd3ade53a9ecd2d911447572f0f6042cc682eb4
Niels Dequeker <niels@newfront.eu>
2021-09-21 10:35:11 +0200

    test(management): add integration test for revoking an Access Grant
    Issue: #1367

---
+++ management-api/pkg/database/access_grant_revoke_test.go
@@ -0,0 +1,109 @@
+// Copyright © VNG Realisatie 2021
+// Licensed under the EUPL
+
+//go:build integration
+// +build integration
+
+package database_test
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/require"
+
+	"go.nlx.io/nlx/management-api/pkg/database"
+)
+
+func TestRevokeAccessGrant(t *testing.T) {
+	t.Parallel()
+
+	setup(t)
+
+	now := time.Now()
+
+	fixtureTime := getFixtureTime(t)
+
+	fixtureCertBundle, err := newFixtureCertificateBundle()
+	require.NoError(t, err)
+
+	fixturePublicKeyPEM, err := fixtureCertBundle.PublicKeyPEM()
+	require.NoError(t, err)
+
+	type args struct {
+		accessGrantID uint
+		revokedAt     time.Time
+	}
+
+	tests := map[string]struct {
+		loadFixtures bool
+		args         args
+		want         *database.AccessGrant
+		wantErr      error
+	}{
+		"when_access_grant_does_not_exist": {
+			loadFixtures: true,
+			args: args{
+				accessGrantID: 42,
+				revokedAt:     now,
+			},
+			want:    nil,
+			wantErr: database.ErrNotFound,
+		},
+		"when_access_grant_is_already_revoked": {
+			loadFixtures: true,
+			args: args{
+				accessGrantID: 2,
+				revokedAt:     now,
+			},
+			want:    nil,
+			wantErr: database.ErrAccessGrantAlreadyRevoked,
+		},
+		"happy_flow": {
+			loadFixtures: true,
+			args: args{
+				accessGrantID: 1,
+				revokedAt:     now,
+			},
+			want: &database.AccessGrant{
+				ID:                      1,
+				IncomingAccessRequestID: 1,
+				IncomingAccessRequest: &database.IncomingAccessRequest{
+					ID:                   1,
+					ServiceID:            1,
+					OrganizationName:     "fixture-organization-name",
+					State:                database.IncomingAccessRequestReceived,
+					CreatedAt:            fixtureTime,
+					UpdatedAt:            fixtureTime,
+					PublicKeyPEM:         fixturePublicKeyPEM,
+					PublicKeyFingerprint: fixtureCertBundle.PublicKeyFingerprint(),
+				},
+				CreatedAt: fixtureTime,
+				RevokedAt: sql.NullTime{Time: now, Valid: true},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for name, tt := range tests {
+		tt := tt
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			configDb, close := newConfigDatabase(t, t.Name(), tt.loadFixtures)
+			defer close()
+
+			got, err := configDb.RevokeAccessGrant(context.Background(), tt.args.accessGrantID, tt.args.revokedAt)
+			require.ErrorIs(t, err, tt.wantErr)
+
+			if tt.wantErr == nil {
+				require.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
--- management-api/pkg/database/postgres_test.go
+++ management-api/pkg/database/postgres_test.go
@@ -6,4 +6,31 @@
 
 package database_test
 
+import (
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	common_tls "go.nlx.io/nlx/common/tls"
+)
+
 const fixturesStartID = 10001
+
+func newFixtureCertificateBundle() (*common_tls.CertificateBundle, error) {
+	pkiDir := filepath.Join("..", "..", "..", "testing", "pki")
+
+	return common_tls.NewBundleFromFiles(
+		filepath.Join(pkiDir, "org-nlx-test-chain.pem"),
+		filepath.Join(pkiDir, "org-nlx-test-key.pem"),
+		filepath.Join(pkiDir, "ca-root.pem"),
+	)
+}
+
+func getFixtureTime(t *testing.T) time.Time {
+	fixtureTime, err := time.Parse(time.RFC3339, "2021-01-02T01:02:03Z")
+	require.NoError(t, err)
+
+	return fixtureTime
+}
