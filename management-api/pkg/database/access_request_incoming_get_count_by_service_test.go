a901c6fa697ef8e18a4a19d6dca0b496ab0dd4bb
Niels Dequeker <niels@newfront.eu>
2021-09-21 15:32:01 +0200

    test(management): add integration test for getting amount of Incoming Access Requests by Service
    Issue: #1367

---
+++ management-api/pkg/database/access_request_incoming_get_count_by_service_test.go
@@ -0,0 +1,54 @@
+// Copyright © VNG Realisatie 2021
+// Licensed under the EUPL
+
+//go:build integration
+// +build integration
+
+package database_test
+
+import (
+	"context"
+	"testing"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetIncomingAccessRequestCountByService(t *testing.T) {
+	t.Parallel()
+
+	setup(t)
+
+	tests := map[string]struct {
+		loadFixtures bool
+		want         map[string]int
+		wantErr      error
+	}{
+		"happy_flow": {
+			loadFixtures: true,
+			want: map[string]int{
+				"fixture-service-name": 1,
+			},
+			wantErr: nil,
+		},
+	}
+
+	for name, tt := range tests {
+		tt := tt
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			configDb, close := newConfigDatabase(t, t.Name(), tt.loadFixtures)
+			defer close()
+
+			got, err := configDb.GetIncomingAccessRequestCountByService(context.Background())
+			require.ErrorIs(t, err, tt.wantErr)
+
+			if tt.wantErr == nil {
+				require.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
