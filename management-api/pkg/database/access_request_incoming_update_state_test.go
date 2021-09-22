b5b2e6f64f458cf18546046eb9b1e7db89df228a
Niels Dequeker <niels@newfront.eu>
2021-09-22 20:58:29 +0200

    test(management): add integration test for updating an Incoming Access Request state
    Issue: #1367

---
+++ management-api/pkg/database/access_request_incoming_update_state_test.go
@@ -0,0 +1,78 @@
+// Copyright © VNG Realisatie 2021
+// Licensed under the EUPL
+
+//go:build integration
+// +build integration
+
+package database_test
+
+import (
+	"context"
+	"testing"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.nlx.io/nlx/management-api/pkg/database"
+)
+
+func TestUpdateIncomingAccessRequestState(t *testing.T) {
+	t.Parallel()
+
+	setup(t)
+
+	type args struct {
+		accessRequestID uint
+		state           database.IncomingAccessRequestState
+	}
+
+	tests := map[string]struct {
+		loadFixtures bool
+		args         args
+		wantErr      error
+	}{
+		"when_access_request_does_not_exist": {
+			loadFixtures: false,
+			args: args{
+				accessRequestID: 1,
+				state:           database.IncomingAccessRequestApproved,
+			},
+			wantErr: database.ErrNotFound,
+		},
+		"happy_flow": {
+			loadFixtures: true,
+			args: args{
+				accessRequestID: 1,
+				state:           database.IncomingAccessRequestApproved,
+			},
+			wantErr: nil,
+		},
+	}
+
+	for name, tt := range tests {
+		tt := tt
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			configDb, close := newConfigDatabase(t, t.Name(), tt.loadFixtures)
+			defer close()
+
+			err := configDb.UpdateIncomingAccessRequestState(context.Background(), tt.args.accessRequestID, tt.args.state)
+			require.ErrorIs(t, err, tt.wantErr)
+
+			if tt.wantErr == nil {
+				asserIncomingAccessRequestState(t, configDb, tt.args.accessRequestID, tt.args.state)
+			}
+		})
+	}
+}
+
+func asserIncomingAccessRequestState(t *testing.T, repo database.ConfigDatabase, accessRequestID uint, state database.IncomingAccessRequestState) {
+	accessRequest, err := repo.GetIncomingAccessRequest(context.Background(), accessRequestID)
+	require.NoError(t, err)
+
+	assert.Equal(t, state, accessRequest.State)
+}
