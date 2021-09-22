4ac7a982a20ce9de0a1a8bc19e9cfc2869316c58
Niels Dequeker <niels@newfront.eu>
2021-09-22 21:08:27 +0200

    test(management): add integration test for creating an Outgoing Access Request
    Issue: #1367

---
+++ management-api/pkg/database/access_request_outgoing_create_test.go
@@ -0,0 +1,102 @@
+// Copyright © VNG Realisatie 2021
+// Licensed under the EUPL
+
+//go:build integration
+// +build integration
+
+package database_test
+
+import (
+	"context"
+	"testing"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/require"
+
+	"go.nlx.io/nlx/management-api/pkg/database"
+)
+
+func TestCreateOutgoingAccessRequest(t *testing.T) {
+	t.Parallel()
+
+	setup(t)
+
+	fixtureTime := getFixtureTime(t)
+
+	fixtureCertBundle, err := newFixtureCertificateBundle()
+	require.NoError(t, err)
+
+	fixturePublicKeyPEM, err := fixtureCertBundle.PublicKeyPEM()
+	require.NoError(t, err)
+
+	type args struct {
+		accessRequest *database.OutgoingAccessRequest
+	}
+
+	tests := map[string]struct {
+		loadFixtures bool
+		args         args
+		want         *database.OutgoingAccessRequest
+		wantErr      error
+	}{
+		"when_an_outgoing_access_request_for_the_service_is_already_present": {
+			loadFixtures: true,
+			args: args{
+				accessRequest: &database.OutgoingAccessRequest{
+					OrganizationName:     "fixture-organization-name",
+					ServiceName:          "fixture-service-name",
+					PublicKeyPEM:         fixturePublicKeyPEM,
+					PublicKeyFingerprint: fixtureCertBundle.PublicKeyFingerprint(),
+					State:                database.OutgoingAccessRequestCreated,
+					CreatedAt:            fixtureTime,
+					UpdatedAt:            fixtureTime,
+				},
+			},
+			wantErr: database.ErrActiveAccessRequest,
+		},
+		"happy_flow": {
+			loadFixtures: false,
+			args: args{
+				accessRequest: &database.OutgoingAccessRequest{
+					OrganizationName:     "my-org",
+					ServiceName:          "my-service",
+					PublicKeyPEM:         fixturePublicKeyPEM,
+					PublicKeyFingerprint: fixtureCertBundle.PublicKeyFingerprint(),
+					State:                database.OutgoingAccessRequestCreated,
+					CreatedAt:            fixtureTime,
+					UpdatedAt:            fixtureTime,
+				},
+			},
+			want: &database.OutgoingAccessRequest{
+				ID:                   fixturesStartID,
+				OrganizationName:     "my-org",
+				ServiceName:          "my-service",
+				PublicKeyPEM:         fixturePublicKeyPEM,
+				PublicKeyFingerprint: fixtureCertBundle.PublicKeyFingerprint(),
+				State:                database.OutgoingAccessRequestCreated,
+				CreatedAt:            fixtureTime,
+				UpdatedAt:            fixtureTime,
+			},
+			wantErr: nil,
+		},
+	}
+
+	for name, tt := range tests {
+		tt := tt
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			configDb, close := newConfigDatabase(t, t.Name(), tt.loadFixtures)
+			defer close()
+
+			got, err := configDb.CreateOutgoingAccessRequest(context.Background(), tt.args.accessRequest)
+			require.ErrorIs(t, err, tt.wantErr)
+
+			if tt.wantErr == nil {
+				require.Equal(t, tt.want, got)
+			}
+		})
+	}
+}
