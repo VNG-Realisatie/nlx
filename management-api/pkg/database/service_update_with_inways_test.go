0348dc58bb149d73cd6cf91662e81b6596a4ece2
Henk van Maanen <henk.van.maanen@aceworks.nl>
2021-09-24 17:11:44 +0200

    test(management): add integration test for updating service with inways

---
+++ management-api/pkg/database/service_update_with_inways_test.go
@@ -0,0 +1,126 @@
+// Copyright © VNG Realisatie 2021
+// Licensed under the EUPL
+
+//go:build integration
+// +build integration
+
+package database_test
+
+import (
+	"context"
+	"testing"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/require"
+
+	"go.nlx.io/nlx/management-api/pkg/database"
+)
+
+func TestUpdateServiceWithInways(t *testing.T) {
+	t.Parallel()
+
+	setup(t)
+
+	fixtureTime := getFixtureTime(t)
+
+	type args struct {
+		service    *database.Service
+		inwayNames []string
+	}
+
+	tests := map[string]struct {
+		loadFixtures bool
+		args         args
+		want         *database.Service
+		wantErr      error
+	}{
+		"when_inway_not_found": {
+			loadFixtures: true,
+			args: args{
+				service: &database.Service{
+					ID:                   1,
+					EndpointURL:          "http://api:8000",
+					DocumentationURL:     "https://documentation-url.com",
+					APISpecificationURL:  "http://api:8000/schema?format=openapi-json",
+					Internal:             true,
+					TechSupportContact:   "test@tech-support-contact.com",
+					PublicSupportContact: "test@public-support-contact.com",
+					OneTimeCosts:         1,
+					MonthlyCosts:         2,
+					RequestCosts:         3,
+				},
+				inwayNames: []string{
+					"arbitrary",
+				},
+			},
+			want:    nil,
+			wantErr: database.ErrNotFound,
+		},
+		"happy_flow": {
+			loadFixtures: true,
+			args: args{
+				service: &database.Service{
+					ID:                   1,
+					EndpointURL:          "http://api:8000",
+					DocumentationURL:     "https://documentation-url.com",
+					APISpecificationURL:  "http://api:8000/schema?format=openapi-json",
+					Internal:             true,
+					TechSupportContact:   "test@tech-support-contact.com",
+					PublicSupportContact: "test@public-support-contact.com",
+					OneTimeCosts:         1,
+					MonthlyCosts:         2,
+					RequestCosts:         3,
+				},
+				inwayNames: []string{
+					"fixture-inway",
+				},
+			},
+			want: &database.Service{
+				ID:                   1,
+				Name:                 "fixture-service-name",
+				EndpointURL:          "http://api:8000",
+				DocumentationURL:     "https://documentation-url.com",
+				APISpecificationURL:  "http://api:8000/schema?format=openapi-json",
+				Internal:             true,
+				TechSupportContact:   "test@tech-support-contact.com",
+				PublicSupportContact: "test@public-support-contact.com",
+				Inways: []*database.Inway{
+					{
+						ID:          1,
+						Name:        "fixture-inway",
+						Version:     "unknown",
+						Hostname:    "fixture-server",
+						IPAddress:   "127.0.0.1",
+						SelfAddress: "fixture.local",
+						CreatedAt:   fixtureTime,
+						UpdatedAt:   fixtureTime,
+					},
+				},
+				OneTimeCosts: 1,
+				MonthlyCosts: 2,
+				RequestCosts: 3,
+				CreatedAt:    fixtureTime,
+			},
+			wantErr: nil,
+		},
+	}
+
+	for name, tt := range tests {
+		tt := tt
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			configDb, close := newConfigDatabase(t, t.Name(), tt.loadFixtures)
+			defer close()
+
+			err := configDb.UpdateServiceWithInways(context.Background(), tt.args.service, tt.args.inwayNames)
+			require.ErrorIs(t, err, tt.wantErr)
+
+			if tt.wantErr == nil {
+				assertServiceWithInways(t, configDb, tt.args.service.Name, tt.want)
+			}
+		})
+	}
+}
