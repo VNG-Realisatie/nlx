706f5110591ea73c81573ee58e3133d5477ccb07
Niels Dequeker <niels@newfront.eu>
2021-10-20 10:58:54 +0200

    fix(management): remove existing incoming/outgoing access requests
    Issue: #1376

--- management-api/migrations/sql/019_add_serial_number.up.sql
+++ management-api/migrations/sql/019_add_serial_number.up.sql
@@ -7,6 +7,10 @@ BEGIN transaction;
 ALTER TABLE nlx_management.access_requests_incoming ADD COLUMN organization_serial_number VARCHAR(20) NOT NULL;
 ALTER TABLE nlx_management.access_requests_outgoing ADD COLUMN organization_serial_number VARCHAR(20) NOT NULL;
 
+-- delete access requests, since we dont have the organization serial number in the old records
+DELETE FROM nlx_management.access_requests_incoming;
+DELETE FROM nlx_management.access_requests_outgoing;
+
 -- delete existing orders, since we dont have the serial numbers of the organization for every service
 DELETE FROM nlx_management.outgoing_orders;
 DELETE FROM nlx_management.incoming_orders;
