cf2e7eb856ea77aa19cbbe2ea57b48ea6e7b77ea
Ronald Koster <ronald.koster07@gmail.com>
2021-10-31 12:18:34 +0100

    fix(management): rework incorrect migrations

--- management-api/migrations/sql/019_add_serial_number.up.sql
+++ management-api/migrations/sql/019_add_serial_number.up.sql
@@ -4,9 +4,6 @@
  */
 BEGIN transaction;
 
-ALTER TABLE nlx_management.access_requests_incoming ADD COLUMN organization_serial_number VARCHAR(20) NOT NULL;
-ALTER TABLE nlx_management.access_requests_outgoing ADD COLUMN organization_serial_number VARCHAR(20) NOT NULL;
-
 -- delete audit logs, since we dont have the organization serial numbers for the services in the old records
 DELETE FROM nlx_management.audit_logs_services;
 DELETE FROM nlx_management.audit_logs;
@@ -18,6 +15,9 @@ DELETE FROM nlx_management.access_requests_outgoing;
 -- delete existing orders, since we dont have the serial numbers of the organization for every service
 DELETE FROM nlx_management.outgoing_orders;
 DELETE FROM nlx_management.incoming_orders;
+
+ALTER TABLE nlx_management.access_requests_incoming ADD COLUMN organization_serial_number VARCHAR(20) NOT NULL;
+ALTER TABLE nlx_management.access_requests_outgoing ADD COLUMN organization_serial_number VARCHAR(20) NOT NULL;
 
 ALTER TABLE nlx_management.incoming_orders_services RENAME COLUMN organization TO organization_name;
 ALTER TABLE nlx_management.incoming_orders_services ADD COLUMN organization_serial_number VARCHAR(20) NOT NULL;
