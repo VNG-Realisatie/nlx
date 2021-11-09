f730a724a0cce6c43e3cd93ed6905a891ab53247
Henk van Maanen <henk.van.maanen@aceworks.nl>
2021-11-09 14:45:27 +0100

    refactor(directory): add stats

---
+++ directory-api/pkg/directory/stats.go
@@ -0,0 +1,58 @@
+// Copyright © VNG Realisatie 2021
+// Licensed under the EUPL
+
+package directory
+
+import (
+	"context"
+	"fmt"
+
+	"go.uber.org/zap"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	directoryapi "go.nlx.io/nlx/directory-api/api"
+	"go.nlx.io/nlx/directory-api/domain"
+)
+
+func (h *DirectoryService) ListInOutwayStatistics(ctx context.Context, _ *emptypb.Empty) (*directoryapi.ListInOutwayStatisticsResponse, error) {
+	h.logger.Info("rpc request ListOrganizations")
+
+	versionStatistics, err := h.repository.ListVersionStatistics(ctx)
+	if err != nil {
+		h.logger.Error("failed to select version statistics from storage", zap.Error(err))
+		return nil, status.New(codes.Internal, "Storage error.").Err()
+	}
+
+	return convertModelToResponse(versionStatistics), nil
+}
+
+func convertModelToResponse(model []*domain.VersionStatistics) *directoryapi.ListInOutwayStatisticsResponse {
+	result := &directoryapi.ListInOutwayStatisticsResponse{}
+
+	for _, statistics := range model {
+		modelType, err := modelTypeToResponseType(statistics.GatewayType())
+		if err != nil {
+			continue
+		}
+
+		result.Versions = append(result.Versions, &directoryapi.ListInOutwayStatisticsResponse_Statistics{
+			Type:    modelType,
+			Version: statistics.Version(),
+			Amount:  statistics.Amount(),
+		})
+	}
+
+	return result
+}
+
+func modelTypeToResponseType(statisticsType domain.VersionStatisticsType) (directoryapi.ListInOutwayStatisticsResponse_Statistics_Type, error) {
+	if statisticsType == domain.TypeInway {
+		return directoryapi.ListInOutwayStatisticsResponse_Statistics_INWAY, nil
+	} else if statisticsType == domain.TypeOutway {
+		return directoryapi.ListInOutwayStatisticsResponse_Statistics_OUTWAY, nil
+	}
+
+	return directoryapi.ListInOutwayStatisticsResponse_Statistics_INWAY, fmt.Errorf("unknown type '%s', expected '%v' or '%v'", statisticsType, domain.TypeInway, domain.TypeOutway)
+}
---
+++ directory-api/pkg/directory/stats_test.go
@@ -0,0 +1,84 @@
+// Copyright © VNG Realisatie 2020
+// Licensed under the EUPL
+
+package directory_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	directoryapi "go.nlx.io/nlx/directory-api/api"
+	"go.nlx.io/nlx/directory-api/domain"
+)
+
+func TestListInOutwayStatistics(t *testing.T) {
+	tests := map[string]struct {
+		setup            func(serviceMocks)
+		expectedResponse *directoryapi.ListInOutwayStatisticsResponse
+		expectedError    error
+	}{
+		"failed_to_get_statistics_from_the_db": {
+			setup: func(s serviceMocks) {
+				s.r.EXPECT().ListVersionStatistics(gomock.Any()).Return(nil, errors.New("arbitrary error"))
+			},
+			expectedResponse: nil,
+			expectedError:    status.New(codes.Internal, "Storage error.").Err(),
+		},
+		"happy flow": {
+			setup: func(s serviceMocks) {
+				versionStatisticsOutway, _ := domain.NewVersionStatistics(&domain.NewVersionStatisticsArgs{
+					GatewayType: domain.TypeOutway,
+					Amount:      5,
+					Version:     "0.0.1",
+				})
+
+				versionStatisticsInway, _ := domain.NewVersionStatistics(&domain.NewVersionStatisticsArgs{
+					GatewayType: domain.TypeInway,
+					Amount:      20,
+					Version:     "0.0.3",
+				})
+
+				s.r.EXPECT().ListVersionStatistics(gomock.Any()).Return([]*domain.VersionStatistics{
+					versionStatisticsOutway,
+					versionStatisticsInway,
+				}, nil)
+			},
+			expectedResponse: &directoryapi.ListInOutwayStatisticsResponse{
+				Versions: []*directoryapi.ListInOutwayStatisticsResponse_Statistics{
+					{
+						Type:    directoryapi.ListInOutwayStatisticsResponse_Statistics_OUTWAY,
+						Amount:  5,
+						Version: "0.0.1",
+					},
+					{
+						Type:    directoryapi.ListInOutwayStatisticsResponse_Statistics_INWAY,
+						Amount:  20,
+						Version: "0.0.3",
+					},
+				},
+			},
+			expectedError: nil,
+		},
+	}
+	for name, tt := range tests {
+		tt := tt
+
+		t.Run(name, func(t *testing.T) {
+			service, mocks := newService(t)
+
+			tt.setup(mocks)
+
+			got, err := service.ListInOutwayStatistics(context.Background(), &emptypb.Empty{})
+
+			assert.Equal(t, tt.expectedResponse, got)
+			assert.Equal(t, tt.expectedError, err)
+		})
+	}
+}
