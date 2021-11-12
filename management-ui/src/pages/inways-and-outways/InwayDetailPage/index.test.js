8d170c3db5bc2b95e2b13a9b672164b7d1977aac
Luciano Nooijen <luciano@bytecode.nl>
2021-11-12 12:25:55 +0200

    feat(management): implement deletion of inway in management UI and handle inway reregister
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- management-ui/src/pages/inways-and-outways/InwayDetailPage/index.test.js
+++ management-ui/src/pages/inways-and-outways/InwayDetailPage/index.test.js
@@ -3,7 +3,8 @@
 //
 import React from 'react'
 import { Route, StaticRouter as Router } from 'react-router-dom'
-import { renderWithProviders } from '../../../test-utils'
+import { renderWithAllProviders } from '../../../test-utils'
+import { RootStore, StoreProvider } from '../../../stores'
 import InwayDetailPage from './index'
 
 /* eslint-disable react/prop-types */
@@ -13,24 +14,34 @@ jest.mock('./InwayDetailPageView', () => ({ inway }) => (
 /* eslint-enable react/prop-types */
 
 test('display inway details', () => {
-  const { getByTestId } = renderWithProviders(
-    <Router location="/inways-and-outways/inways/forty-two">
-      <Route path="/inways-and-outways/inways/:name">
-        <InwayDetailPage inway={{ name: 'forty-two' }} />
-      </Route>
-    </Router>,
-  )
+  const rootStore = new RootStore({})
 
+  const { getByTestId } = renderWithAllProviders(
+    <StoreProvider rootStore={rootStore}>
+      <Router location="/inways-and-outways/inways/forty-two">
+        <Route path="/inways-and-outways/inways/:name">
+          <InwayDetailPage
+            inway={{ name: 'forty-two' }}
+            parentUrl="/inways-and-outways"
+          />
+        </Route>
+      </Router>
+    </StoreProvider>,
+  )
   expect(getByTestId('inway-details')).toHaveTextContent('forty-two')
 })
 
 test('display a non-existing inway', async () => {
-  const { findByTestId } = renderWithProviders(
-    <Router location="/inways-and-outways/inways/forty-two">
-      <Route path="/inways-and-outways/inways/:name">
-        <InwayDetailPage inway={null} />
-      </Route>
-    </Router>,
+  const rootStore = new RootStore({})
+
+  const { findByTestId } = renderWithAllProviders(
+    <StoreProvider rootStore={rootStore}>
+      <Router location="/inways-and-outways/inways/forty-two">
+        <Route path="/inways-and-outways/inways/:name">
+          <InwayDetailPage inway={null} parentUrl="/inways-and-outways" />
+        </Route>
+      </Router>
+    </StoreProvider>,
   )
 
   const message = await findByTestId('error-message')
