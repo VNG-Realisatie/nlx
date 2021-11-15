48cc3e423424be95589735f80c868db7bc63c634
Ronald Koster <ronald.koster07@gmail.com>
2021-11-15 09:41:43 +0100

    refactor(directory): register in seperate functions

--- directory-api/pkg/directory/inway.go
+++ directory-api/pkg/directory/inway.go
@@ -14,16 +14,14 @@ import (
 	"google.golang.org/grpc/status"
 
 	"go.nlx.io/nlx/common/nlxversion"
+	"go.nlx.io/nlx/common/tls"
 	directoryapi "go.nlx.io/nlx/directory-api/api"
 	"go.nlx.io/nlx/directory-api/domain"
 )
 
 const maxServiceCount = 250
 
-//nolint // @TODO fix this before merging
 func (h *DirectoryService) RegisterInway(ctx context.Context, req *directoryapi.RegisterInwayRequest) (*directoryapi.RegisterInwayResponse, error) {
-	logger := h.logger.With(zap.String("handler", "register-inway"))
-
 	resp := &directoryapi.RegisterInwayResponse{}
 
 	organizationInformation, err := h.getOrganizationInformationFromRequest(ctx)
@@ -63,22 +61,10 @@ func (h *DirectoryService) RegisterInway(ctx context.Context, req *directoryapi.
 		return nil, status.New(codes.Internal, "failed to register inway").Err()
 	}
 
-	if inwayModel.IsOrganizationInway() {
-		h.logger.Debug("is organization inway", zap.String("inway", inwayModel.ToString()))
-		err = h.repository.SetOrganizationInway(ctx, organizationInformation.SerialNumber, inwayModel.Address())
-
-		if err != nil {
-			h.logger.Error("set organization inway", zap.String("inway", inwayModel.ToString()), zap.Error(err))
-			return nil, status.New(codes.Internal, "failed to set organization inway").Err()
-		}
-	} else {
-		h.logger.Debug("is not organization inway", zap.String("inway", inwayModel.ToString()))
-		err = h.repository.ClearIfSetAsOrganizationInway(ctx, organizationInformation.SerialNumber, inwayModel.Address())
-
-		if err != nil {
-			h.logger.Error("failed to execute ClearIfSetAsOrganizationInway", zap.String("inway", inwayModel.ToString()), zap.Error(err))
-			return nil, status.New(codes.Internal, "failed to update organization inway settings").Err()
-		}
+	err = h.setOrganizationInway(ctx, organizationInformation, inwayModel)
+	if err != nil {
+		h.logger.Error("failed to configure organization inway", zap.String("inway", inwayModel.ToString()), zap.Error(err))
+		return nil, status.New(codes.Internal, "failed to set organization inway ").Err()
 	}
 
 	if len(req.Services) > maxServiceCount {
@@ -86,51 +72,80 @@ func (h *DirectoryService) RegisterInway(ctx context.Context, req *directoryapi.
 	}
 
 	for _, s := range req.Services {
-		s := s
-
-		serviceSpecificationType := getAPISpecificationTypeForService(
-			h.httpClient,
-			h.logger,
-			s.ApiSpecificationDocumentUrl,
-			req.InwayAddress,
-			s.Name,
-		)
-
-		organization, err := domain.NewOrganization(organizationInformation.Name, organizationInformation.SerialNumber)
+		err = h.registerService(organizationInformation, req.InwayAddress, s)
 		if err != nil {
-			msg := fmt.Sprintf("validation for organization with serial number '%s' failed: %s", organizationInformation.SerialNumber, err.Error())
-			return nil, status.New(codes.InvalidArgument, msg).Err()
-		}
-
-		serviceModel, err := domain.NewService(
-			&domain.NewServiceArgs{
-				Name:                 s.Name,
-				Organization:         organization,
-				Internal:             s.Internal,
-				DocumentationURL:     s.DocumentationUrl,
-				APISpecificationType: serviceSpecificationType,
-				PublicSupportContact: s.PublicSupportContact,
-				TechSupportContact:   s.TechSupportContact,
-				Costs: &domain.NewServiceCostsArgs{
-					OneTime: uint(s.OneTimeCosts),
-					Monthly: uint(s.MonthlyCosts),
-					Request: uint(s.RequestCosts),
-				},
-			},
-		)
-		if err != nil {
-			msg := fmt.Sprintf("validation for service named '%s' failed: %s", s.Name, err.Error())
-			return nil, status.New(codes.InvalidArgument, msg).Err()
-		}
-
-		err = h.repository.RegisterService(serviceModel)
-		if err != nil {
-			logger.Error("failed to register service", zap.Error(err))
-			return nil, status.New(codes.Internal, "database error").Err()
+			return nil, err
 		}
 	}
 
 	return resp, nil
+}
+
+func (h *DirectoryService) setOrganizationInway(ctx context.Context, organizationInformation *tls.OrganizationInformation, inwayModel *domain.Inway) error {
+	if inwayModel.IsOrganizationInway() {
+		h.logger.Debug("is organization inway", zap.String("inway", inwayModel.ToString()))
+		err := h.repository.SetOrganizationInway(ctx, organizationInformation.SerialNumber, inwayModel.Address())
+
+		if err != nil {
+			h.logger.Error("set organization inway", zap.String("inway", inwayModel.ToString()), zap.Error(err))
+			return status.New(codes.Internal, "failed to set organization inway").Err()
+		}
+	} else {
+		h.logger.Debug("is not organization inway", zap.String("inway", inwayModel.ToString()))
+		err := h.repository.ClearIfSetAsOrganizationInway(ctx, organizationInformation.SerialNumber, inwayModel.Address())
+
+		if err != nil {
+			h.logger.Error("failed to execute ClearIfSetAsOrganizationInway", zap.String("inway", inwayModel.ToString()), zap.Error(err))
+			return status.New(codes.Internal, "failed to update organization inway settings").Err()
+		}
+	}
+
+	return nil
+}
+
+func (h *DirectoryService) registerService(organizationInformation *tls.OrganizationInformation, inwayAddress string, service *directoryapi.RegisterInwayRequest_RegisterService) error {
+	serviceSpecificationType := getAPISpecificationTypeForService(
+		h.httpClient,
+		h.logger,
+		service.ApiSpecificationDocumentUrl,
+		inwayAddress,
+		service.Name,
+	)
+
+	organization, err := domain.NewOrganization(organizationInformation.Name, organizationInformation.SerialNumber)
+	if err != nil {
+		msg := fmt.Sprintf("validation for organization with serial number '%s' failed: %s", organizationInformation.SerialNumber, err.Error())
+		return status.New(codes.InvalidArgument, msg).Err()
+	}
+
+	serviceModel, err := domain.NewService(
+		&domain.NewServiceArgs{
+			Name:                 service.Name,
+			Organization:         organization,
+			Internal:             service.Internal,
+			DocumentationURL:     service.DocumentationUrl,
+			APISpecificationType: serviceSpecificationType,
+			PublicSupportContact: service.PublicSupportContact,
+			TechSupportContact:   service.TechSupportContact,
+			Costs: &domain.NewServiceCostsArgs{
+				OneTime: uint(service.OneTimeCosts),
+				Monthly: uint(service.MonthlyCosts),
+				Request: uint(service.RequestCosts),
+			},
+		},
+	)
+	if err != nil {
+		msg := fmt.Sprintf("validation for service named '%s' failed: %s", service.Name, err.Error())
+		return status.New(codes.InvalidArgument, msg).Err()
+	}
+
+	err = h.repository.RegisterService(serviceModel)
+	if err != nil {
+		h.logger.Error("failed to register service", zap.Error(err))
+		return status.New(codes.Internal, "database error").Err()
+	}
+
+	return nil
 }
 
 func getAPISpecificationTypeForService(httpClient *http.Client, logger *zap.Logger, specificationDocumentURL, inwayAddress, serviceName string) domain.SpecificationType {
