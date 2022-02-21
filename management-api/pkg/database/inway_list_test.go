913203f56612cdc4a57b08abcb3b94a9bc484cf0
ndequeker <niels.dequeker@fastmail.com>
2022-02-21 13:26:09 +0100

    test(management): add test for listing all Inways
    Issue: #1427

---
+++ management-api/pkg/database/inway_list_test.go
@@ -0,0 +1,121 @@
+// Copyright © VNG Realisatie 2022
+// Licensed under the EUPL
+
+//go:build integration
+
+package database_test
+
+import (
+	"context"
+	"testing"
+
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/require"
+
+	"go.nlx.io/nlx/management-api/pkg/database"
+)
+
+func TestListInways(t *testing.T) {
+	t.Parallel()
+
+	setup(t)
+
+	fixtureTime := getFixtureTime(t)
+
+	tests := map[string]struct {
+		loadFixtures bool
+		want         []*database.Inway
+		wantErr      error
+	}{
+		"no_inways_present": {
+			loadFixtures: false,
+			want:         []*database.Inway{},
+			wantErr:      nil,
+		},
+		"happy_flow": {
+			loadFixtures: true,
+			want: []*database.Inway{
+				{
+					ID:          1,
+					Name:        "fixture-inway",
+					Version:     "unknown",
+					Hostname:    "fixture-server",
+					IPAddress:   "127.0.0.1",
+					SelfAddress: "fixture.local",
+					Services: []*database.Service{
+						{
+							ID:                   1,
+							Name:                 "fixture-service-name",
+							EndpointURL:          "http://fixture-api:8000",
+							DocumentationURL:     "https://fixture-documentation-url.com",
+							APISpecificationURL:  "http://fixture-api:8000/schema?format=openapi-json",
+							Internal:             false,
+							TechSupportContact:   "fixture@tech-support-contact.com",
+							PublicSupportContact: "fixture@public-support-contact.com",
+							Inways:               nil,
+							OneTimeCosts:         1,
+							MonthlyCosts:         2,
+							RequestCosts:         3,
+							CreatedAt:            fixtureTime,
+							UpdatedAt:            fixtureTime,
+						},
+						{
+							ID:                   2,
+							Name:                 "fixture-service-name-two",
+							EndpointURL:          "http://fixture-api:8000",
+							DocumentationURL:     "https://fixture-documentation-url.com",
+							APISpecificationURL:  "http://fixture-api:8000/schema?format=openapi-json",
+							Internal:             false,
+							TechSupportContact:   "fixture@tech-support-contact.com",
+							PublicSupportContact: "fixture@public-support-contact.com",
+							Inways:               nil,
+							OneTimeCosts:         1,
+							MonthlyCosts:         2,
+							RequestCosts:         3,
+							CreatedAt:            fixtureTime,
+							UpdatedAt:            fixtureTime,
+						},
+						{
+							ID:                   3,
+							Name:                 "fixture-service-name-three",
+							EndpointURL:          "http://fixture-api:8000",
+							DocumentationURL:     "https://fixture-documentation-url.com",
+							APISpecificationURL:  "http://fixture-api:8000/schema?format=openapi-json",
+							Internal:             false,
+							TechSupportContact:   "fixture@tech-support-contact.com",
+							PublicSupportContact: "fixture@public-support-contact.com",
+							Inways:               nil,
+							OneTimeCosts:         1,
+							MonthlyCosts:         2,
+							RequestCosts:         3,
+							CreatedAt:            fixtureTime,
+							UpdatedAt:            fixtureTime,
+						},
+					},
+					CreatedAt: fixtureTime,
+					UpdatedAt: fixtureTime,
+				},
+			},
+			wantErr: nil,
+		},
+	}
+
+	for name, tt := range tests {
+		tt := tt
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			configDb, close := newConfigDatabase(t, t.Name(), tt.loadFixtures)
+			defer close()
+
+			got, err := configDb.ListInways(context.Background())
+			require.ErrorIs(t, err, tt.wantErr)
+
+			if tt.wantErr == nil {
+				require.EqualValues(t, tt.want, got)
+			}
+		})
+	}
+}
