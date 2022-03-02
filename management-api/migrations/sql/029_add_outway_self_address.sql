235ed736758d9a38791c324f16d96d9ba994c711
Ronald Koster <ronald.koster07@gmail.com>
2022-03-02 14:40:04 +0100

    fix(management): add default value outway self address to migration

--- management-api/migrations/sql/029_add_outway_self_address.sql
+++ management-api/migrations/sql/029_add_outway_self_address.sql
@@ -1,5 +1,6 @@
 BEGIN transaction;
 
-ALTER TABLE nlx_management.outways ADD COLUMN self_address VARCHAR(100) NOT NULL;
+ALTER TABLE nlx_management.outways ADD COLUMN self_address VARCHAR(100) NOT NULL DEFAULT '';
+;
 
 COMMIT;
