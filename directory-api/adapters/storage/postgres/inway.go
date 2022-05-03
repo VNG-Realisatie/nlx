8a8a1857341cb20808df66f005617c3c99bad309
ndequeker <niels.dequeker@fastmail.com>
2022-05-03 12:39:11 +0200

    refactor(directory): extract converting row to model into a function
    To improve readability
    
    Issue: nlx#1518

--- directory-api/adapters/storage/postgres/inway.go
+++ directory-api/adapters/storage/postgres/inway.go
@@ -20,18 +20,22 @@ func (r *PostgreSQLRepository) GetInway(name, serialNumber string) (*domain.Inwa
 		return nil, fmt.Errorf("failed to get inway (name: %s, serialNumber: %s): %s", name, serialNumber, err)
 	}
 
-	organizationModel, err := domain.NewOrganization(inway.OrganizationName, inway.OrganizationSerialNumber)
+	return convertInwayRowToModel(inway)
+}
+
+func convertInwayRowToModel(row *queries.GetInwayRow) (*domain.Inway, error) {
+	organizationModel, err := domain.NewOrganization(row.OrganizationName, row.OrganizationSerialNumber)
 	if err != nil {
 		return nil, fmt.Errorf("invalid organization model in database: %v", err)
 	}
 
 	model, err := domain.NewInway(&domain.NewInwayArgs{
-		Name:         inway.Name,
+		Name:         row.Name,
 		Organization: organizationModel,
-		Address:      inway.Address,
-		NlxVersion:   inway.NlxVersion,
-		CreatedAt:    inway.CreatedAt,
-		UpdatedAt:    inway.UpdatedAt,
+		Address:      row.Address,
+		NlxVersion:   row.NlxVersion,
+		CreatedAt:    row.CreatedAt,
+		UpdatedAt:    row.UpdatedAt,
 	})
 	if err != nil {
 		return nil, fmt.Errorf("invalid inway model in database: %v", err)
