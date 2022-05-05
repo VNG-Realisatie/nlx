88e5e694526854ad9d098a94b19c4bcd20cbf390
ndequeker <niels.dequeker@fastmail.com>
2022-05-05 09:44:36 +0200

    refactor(directory): move the ClearIfSetAsOrganizationInway method for readability
    Issue: nlx#1518

--- directory-api/adapters/storage/postgres/organization.go
+++ directory-api/adapters/storage/postgres/organization.go
@@ -8,6 +8,8 @@ import (
 	"database/sql"
 	"errors"
 
+	"go.uber.org/zap"
+
 	"go.nlx.io/nlx/directory-api/adapters/storage/postgres/queries"
 	"go.nlx.io/nlx/directory-api/domain"
 	"go.nlx.io/nlx/directory-api/domain/directory/storage"
@@ -21,6 +23,29 @@ func (r *PostgreSQLRepository) ClearOrganizationInway(ctx context.Context, organ
 
 	if rowsAffected != 1 {
 		return storage.ErrNotFound
+	}
+
+	return nil
+}
+
+// ClearIfSetAsOrganizationInway clears the inway for the given organization.
+// This method should be called if IsOrganizationInway is false in the request, to ensure the directory has this correctly set as well
+func (r *PostgreSQLRepository) ClearIfSetAsOrganizationInway(ctx context.Context, serialNumber, selfAddress string) error {
+	organizationSelfAddress, err := r.GetOrganizationInwayAddress(ctx, serialNumber)
+	if err != nil {
+		if errors.Is(err, storage.ErrNotFound) {
+			return nil
+		}
+
+		return err
+	}
+
+	if selfAddress == organizationSelfAddress {
+		r.logger.Warn("unexpected state: inway was incorrectly set as organization inway ",
+			zap.String("inway self address", selfAddress),
+			zap.String("organization inway self address", organizationSelfAddress))
+
+		return r.ClearOrganizationInway(ctx, serialNumber)
 	}
 
 	return nil
