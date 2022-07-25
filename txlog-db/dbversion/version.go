d510ffbd062baee1c4297d7379121f4786ae97da
ndequeker <niels.dequeker@fastmail.com>
2022-07-25 14:17:49 +0200

    feat(txlog): introduce initContainer for txlog-api migrations + remove job-transaction-log.yaml
    Issue: nlx#1579

--- txlog-db/dbversion/version.go
+++
@@ -1,8 +0,0 @@
-// Copyright © VNG Realisatie 2018
-// Licensed under the EUPL
-
-package dbversion
-
-// LatestTxlogDBVersion is the latest version for the db migrations.
-// Components that use the database should verify using the migrate package their
-const LatestTxlogDBVersion = 4
