b952ab642b4851ce3d4302cb78c85e4c4897d2c6
ndequeker <niels.dequeker@fastmail.com>
2022-08-04 16:03:11 +0200

    refactor(management): split settings test files per method
    Issue: nlx#1487

---
+++ management-api/pkg/server/settings_get_test.go
@@ -0,0 +1,79 @@
+// Copyright © VNG Realisatie 2020
+// Licensed under the EUPL
+
+package server_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+
+	"go.nlx.io/nlx/management-api/api"
+	"go.nlx.io/nlx/management-api/domain"
+)
+
+func TestManagementService_GetSettings(t *testing.T) {
+	tests := map[string]struct {
+		setup   func(context.Context, serviceMocks)
+		ctx     context.Context
+		want    *api.Settings
+		wantErr error
+	}{
+		"missing_required_permission": {
+			setup:   func(ctx context.Context, mocks serviceMocks) {},
+			ctx:     testCreateUserWithoutPermissionsContext(),
+			want:    nil,
+			wantErr: status.New(codes.PermissionDenied, "user needs the permission \"permissions.organization_settings.read\" to execute this request").Err(),
+		},
+		"when_the_database_call_fails": {
+			setup: func(ctx context.Context, mocks serviceMocks) {
+				mocks.db.
+					EXPECT().
+					GetSettings(gomock.Any()).
+					Return(nil, errors.New("arbitrary error"))
+
+			},
+			ctx:     testCreateAdminUserContext(),
+			want:    nil,
+			wantErr: status.Error(codes.Internal, "database error"),
+		},
+		"happy flow": {
+			setup: func(ctx context.Context, mocks serviceMocks) {
+				settings, err := domain.NewSettings("inway-name", "mock@email.com")
+				require.NoError(t, err)
+
+				mocks.db.
+					EXPECT().
+					GetSettings(gomock.Any()).
+					Return(settings, nil)
+			},
+			ctx: testCreateAdminUserContext(),
+			want: &api.Settings{
+				OrganizationInway:        "inway-name",
+				OrganizationEmailAddress: "mock@email.com",
+			},
+			wantErr: nil,
+		},
+	}
+
+	for name, tt := range tests {
+		tt := tt
+
+		t.Run(name, func(t *testing.T) {
+			service, _, mocks := newService(t)
+			tt.setup(tt.ctx, mocks)
+
+			got, err := service.GetSettings(tt.ctx, &emptypb.Empty{})
+
+			assert.Equal(t, tt.want, got)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
--- management-api/pkg/server/settings_update_test.go
+++ management-api/pkg/server/settings_update_test.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -21,65 +20,6 @@ import (
 	"go.nlx.io/nlx/management-api/domain"
 	"go.nlx.io/nlx/management-api/pkg/database"
 )
-
-func TestManagementService_GetSettings(t *testing.T) {
-	tests := map[string]struct {
-		setup   func(context.Context, serviceMocks)
-		ctx     context.Context
-		want    *api.Settings
-		wantErr error
-	}{
-		"missing_required_permission": {
-			setup:   func(ctx context.Context, mocks serviceMocks) {},
-			ctx:     testCreateUserWithoutPermissionsContext(),
-			want:    nil,
-			wantErr: status.New(codes.PermissionDenied, "user needs the permission \"permissions.organization_settings.read\" to execute this request").Err(),
-		},
-		"when_the_database_call_fails": {
-			setup: func(ctx context.Context, mocks serviceMocks) {
-				mocks.db.
-					EXPECT().
-					GetSettings(gomock.Any()).
-					Return(nil, errors.New("arbitrary error"))
-
-			},
-			ctx:     testCreateAdminUserContext(),
-			want:    nil,
-			wantErr: status.Error(codes.Internal, "database error"),
-		},
-		"happy flow": {
-			setup: func(ctx context.Context, mocks serviceMocks) {
-				settings, err := domain.NewSettings("inway-name", "mock@email.com")
-				require.NoError(t, err)
-
-				mocks.db.
-					EXPECT().
-					GetSettings(gomock.Any()).
-					Return(settings, nil)
-			},
-			ctx: testCreateAdminUserContext(),
-			want: &api.Settings{
-				OrganizationInway:        "inway-name",
-				OrganizationEmailAddress: "mock@email.com",
-			},
-			wantErr: nil,
-		},
-	}
-
-	for name, tt := range tests {
-		tt := tt
-
-		t.Run(name, func(t *testing.T) {
-			service, _, mocks := newService(t)
-			tt.setup(tt.ctx, mocks)
-
-			got, err := service.GetSettings(tt.ctx, &emptypb.Empty{})
-
-			assert.Equal(t, tt.want, got)
-			assert.Equal(t, tt.wantErr, err)
-		})
-	}
-}
 
 func TestManagementService_UpdateSettings(t *testing.T) {
 	tests := map[string]struct {
