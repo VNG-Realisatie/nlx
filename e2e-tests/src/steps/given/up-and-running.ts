d5f0766d4fdd6a174f7f27d286f03f63e85c915d
ndequeker <niels.dequeker@fastmail.com>
2022-08-09 09:37:35 +0200

    test(management): add extra logging to the up-and-running E2E-test
    Issue: nlx#1171

--- e2e-tests/src/steps/given/up-and-running.ts
+++ e2e-tests/src/steps/given/up-and-running.ts
@@ -6,25 +6,33 @@ import {
   hasDefaultInwayRunning,
   setDefaultInwayAsOrganizationInway,
 } from "../../utils/inway";
+import { hasOutwayRunning } from "../../utils/outway";
+import { default as logger } from "../../debug";
 import { Given } from "@cucumber/cucumber";
-import {hasOutwayRunning} from "../../utils/outway";
+const debug = logger("e2e-tests:up-and-running");
 
 Given(
   "{string} is up and running",
   async function (this: CustomWorld, orgName: string) {
     await authenticate(this, orgName);
+    debug(`authentication for ${orgName} succeeded`);
     await acceptToS(this, orgName);
+    debug(`ToS accepted for ${orgName}`);
 
     const org = getOrgByName(orgName);
 
     if (org.defaultInway.name !== "") {
       await hasDefaultInwayRunning(this, orgName);
+      debug(`default Inway is running for ${orgName}`);
       await setDefaultInwayAsOrganizationInway(this, orgName);
+      debug(`set default Inway as organization Inway for ${orgName}`);
     }
 
-    if (org.outways !== null){
-      for (const outwayName in org.outways){
-        await hasOutwayRunning(this, orgName, outwayName)
+    if (org.outways !== null) {
+      for (const outwayName in org.outways) {
+        debug(`waiting until Outway ${outwayName} is running for ${orgName}`);
+        await hasOutwayRunning(this, orgName, outwayName);
+        debug(`Outway ${outwayName} is running for ${orgName}`);
       }
     }
   }
