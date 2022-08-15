2bd4f7ec0e67b775a0b0889d0ec5bd96dafdcbb5
Ronald Koster <ronald.koster07@gmail.com>
2022-08-15 10:41:14 +0200

    ci: update helm chart lock files
    Issue: nlx#1171

--- scripts/update-conditional-helm-deps.sh
+++ scripts/update-conditional-helm-deps.sh
@@ -2,4 +2,9 @@
 
 helm repo add stable https://charts.helm.sh/stable && \
 helm repo update && \
-helm dependency update helm/deploy/gemeente-stijns
+helm dependency update helm/deploy/directory && \
+helm dependency update helm/deploy/gemeente-riemer && \
+helm dependency update helm/deploy/gemeente-stijns && \
+helm dependency update helm/deploy/vergunningsoftware-bv && \
+helm dependency update helm/deploy/shared
+
