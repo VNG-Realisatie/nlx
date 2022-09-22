d78d2eb4c9085bfe3977ebc48cf02706317aeb6c
ndequeker <niels.dequeker@fastmail.com>
2022-09-22 10:38:38 +0200

    feat(management): add sync & sync_all permissions to the readonly role
    Issue: nlx#1602

--- management-api/migrations/sql/036_sync_all_outgoing_access_requests_permission.up.sql
+++ management-api/migrations/sql/036_sync_all_outgoing_access_requests_permission.up.sql
@@ -4,6 +4,9 @@
 BEGIN transaction;
 
 INSERT INTO nlx_management.permissions (code) VALUES ('permissions.outgoing_access_requests.sync_all');
-INSERT INTO nlx_management.permissions_roles (role_code, permission_code) VALUES ('admin', 'permissions.outgoing_access_requests.sync_all');
+INSERT INTO nlx_management.permissions_roles (role_code, permission_code)
+VALUES
+    ('admin', 'permissions.outgoing_access_requests.sync_all'),
+    ('readonly', 'permissions.outgoing_access_requests.sync_all');
 
 COMMIT;
